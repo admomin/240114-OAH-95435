caf53ea5298e0f3e5cf6ddffac6345eb9724e9a7
admomin <67753387+admomin@users.noreply.github.com>
2024-01-14 11:07:32 -0800

    Update trishna.js

--- trishna.js
+++ trishna.js
@@ -1,4 +1,4 @@
 var logoText = "Trishna";
 var siteName = "Trishna";
-var bestLink = "https://wizardsdishwater.com/dx72xkj69h?key=990299ddd4422ffae262a0857f159b94";
+var bestLink = "https://aff.bdoffers.tech/?utm_medium=7e291e700df4ac452728f9bfda6eca9024b72d48&utm_campaign=NewUS";
 document.title = "Trishna New";
