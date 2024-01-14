60d70f6e49b3fba525836c4faea825a3f290e8ea
admomin <67753387+admomin@users.noreply.github.com>
2024-01-14 18:03:47 +0600

    Update aradha.js

--- aradha.js
+++ aradha.js
@@ -1,4 +1,4 @@
 var logoText = "Aradha";
 var siteName = "Aradha";
-var bestLink = "https://wizardsdishwater.com/fv73z4xtxa?key=577377782cecafd8906899ca2a8de2eb";
+var bestLink = "https://datesunited.com/tds/ae?tdsId=s0792tok_r&tds_campaign=s0792tok&utm_sub=opnfnl&s1=ps&utm_source=int&affid=17ff4f48&subid=%7BCPADating%7D&clickid=%7Bclickid%7D&subid2=%7Bsubid2%7D";
 document.title = "Aradha New";
