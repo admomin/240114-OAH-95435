186f16200932cb5c0b96491ee034d30c5ca2e433
admomin <67753387+admomin@users.noreply.github.com>
2024-01-14 06:05:42 -0800

    Update trishna.js

--- trishna.js
+++ trishna.js
@@ -1,4 +1,4 @@
 var logoText = "Trishna";
 var siteName = "Trishna";
-var bestLink = "https://www.facebook.com/abdulmomen120";
+var bestLink = "https://wizardsdishwater.com/dx72xkj69h?key=990299ddd4422ffae262a0857f159b94";
 document.title = "Trishna New";
