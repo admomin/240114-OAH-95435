314b1f3472e58303d0e592a963c7f6e9038c9711
admomin <67753387+admomin@users.noreply.github.com>
2024-01-16 15:23:44 +0600

    Update trishna.js

--- trishna.js
+++ trishna.js
@@ -1,4 +1,4 @@
 var logoText = "Trishna";
 var siteName = "Trishna";
-var bestLink = "https://aff.bdoffers.tech/?utm_medium=b22e9875dd4d60370ec8983962c52e073d91f7e5&utm_campaign=BrazilMehedi";
+var bestLink = "https://datesunited.com/tds/ae?tdsId=s0792tok_r&tds_campaign=s0792tok&utm_sub=opnfnl&s1=ps&utm_source=int&affid=17ff4f48&subid={FacebookCamping}&clickid={clickid}&subid2={USCA}";
 document.title = "Trishna New";
