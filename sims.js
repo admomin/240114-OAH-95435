a0d5d2959e773ef503700a6055c14190289b6377
admomin <67753387+admomin@users.noreply.github.com>
2024-01-16 14:48:38 +0600

    Update sims.js

--- sims.js
+++ sims.js
@@ -2,4 +2,4 @@ var gp = "https://aff.bdoffers.tech/?utm_medium=2628863530b254f841e2c3f3a8c7f040
 var bl = "https://aff.bdoffers.tech/?utm_medium=d328b8e0b6bda56875482a4cfd21b91a3e52c93a&utm_campaign=ULFAT_BANGLALINK";
 var robi = "https://aff.bdoffers.tech/?utm_medium=6ed6fd5eddbf6dd33e94f596e9d514f5e847aec3&utm_campaign=ULFAT_ROBI";
 var artl = "https://aff.bdoffers.tech/?utm_medium=bd20dbbb9f721be3b7dcc87b0a153e69ba4032b0&utm_campaign=ULFAT_Airtel";
-var tl = "https://aff.bdoffers.tech/?utm_medium=a4a65fef67093801a6b2b1f9ae6512841b10e458&utm_campaign=AIRTEL_ROBI";
+var tl = "https://gozoboz.org/cl/a9d54f6ca3f4c4e9?p1=&p2=&source=facebook&site=Momin";
