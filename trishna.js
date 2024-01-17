7d893620658d9e0b51d6ee6d0f5e8c65f2086569
admomin <67753387+admomin@users.noreply.github.com>
2024-01-17 09:20:47 +0600

    Update trishna.js

--- trishna.js
+++ trishna.js
@@ -1,4 +1,4 @@
 var logoText = "Trishna";
 var siteName = "Trishna";
-var bestLink = "https://datesunited.com/tds/ae?tdsId=s0792tok_r&tds_campaign=s0792tok&utm_sub=opnfnl&s1=ps&utm_source=int&affid=17ff4f48&subid={FacebookCamping}&clickid={clickid}&subid2={USCA}";
+var bestLink = "https://wizardsdishwater.com/dx72xkj69h?key=990299ddd4422ffae262a0857f159b94";
 document.title = "Trishna New";
