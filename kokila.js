7695f9b0cfaa9fbe1d49134ad187064b04db95a5
admomin <67753387+admomin@users.noreply.github.com>
2024-01-17 09:20:29 +0600

    Update kokila.js

--- kokila.js
+++ kokila.js
@@ -1,4 +1,4 @@
 var logoText = "Kokila";
 var siteName = "Kokila";
-var bestLink = "https://datesunited.com/tds/ae?tdsId=s0792tok_r&tds_campaign=s0792tok&utm_sub=opnfnl&s1=ps&utm_source=int&affid=17ff4f48&subid={xmx}&clickid={clickid}&subid2={subid2}";
+var bestLink = "https://wizardsdishwater.com/dx72xkj69h?key=990299ddd4422ffae262a0857f159b94";
 document.title = "Kokila New";
