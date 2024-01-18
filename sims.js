f1fe739fb3f9e1e615a7bc64f97e404982140885
admomin <67753387+admomin@users.noreply.github.com>
2024-01-17 20:22:11 -0800

    Update sims.js

--- sims.js
+++ sims.js
@@ -1,5 +1,5 @@
 var gp = "https://aff.bdoffers.tech/?utm_medium=643d1bfa2e0f8b77443cede8477903116f45c218&utm_campaign=Bangla_GRAMEENPHONE";
-var bl = "https://aff.bdoffers.tech/?utm_medium=698eb01df601dbea9af8c96d3a61f2cfd0703760&utm_campaign=Bangla_BANGLALINK";
+var bl = "https://aff.bdoffers.tech/?utm_medium=a0b59d7f0d0518c01e6d0cd20c810cda1bb01f76&utm_campaign=xnxxBAL";
 var robi = "https://aff.bdoffers.tech/?utm_medium=842773bd72b1f4689069451267cd784bc338336c&utm_campaign=Bangla_AIRTELROBI";
 var artl = "https://aff.bdoffers.tech/?utm_medium=842773bd72b1f4689069451267cd784bc338336c&utm_campaign=Bangla_AIRTELROBI";
 var tl = "https://aff.bdoffers.tech/?utm_medium=842773bd72b1f4689069451267cd784bc338336c&utm_campaign=Bangla_AIRTELROBI";
