5793582c4516ddb1ba88c43d5e07b45e459712bc
admomin <67753387+admomin@users.noreply.github.com>
2024-01-19 21:12:50 +0600

    Update trishna.js

--- trishna.js
+++ trishna.js
@@ -1,4 +1,4 @@
 var logoText = "Trishna";
 var siteName = "Trishna";
-var bestLink = "https://hautoust.com/4/6937685";
+var bestLink = "https://wizardsdishwater.com/dx72xkj69h?key=990299ddd4422ffae262a0857f159b94";
 document.title = "Trishna New";
