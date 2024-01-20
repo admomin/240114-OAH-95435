a119e7b570e4845292ebbbf8e3de4aa76ff61c82
admomin <67753387+admomin@users.noreply.github.com>
2024-01-20 12:03:35 -0600

    Update tumrinageetika.js

--- tumrinageetika.js
+++ tumrinageetika.js
@@ -1,4 +1,4 @@
 var logoText = "Tumrina";
 var siteName = "Tumrina";
-var bestLink = "https://duckist.me";
+var bestLink = "https://wizardsdishwater.com/vs1x112dn0?key=28dcb4c360c99f75c117c61d0a61f495";
 document.title = "Click Here To Watch";
