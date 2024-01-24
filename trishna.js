31f99bf825897701b8a2dc041a07d99030e732c9
admomin <67753387+admomin@users.noreply.github.com>
2024-01-24 08:22:46 -0800

    Update trishna.js

--- trishna.js
+++ trishna.js
@@ -1,4 +1,4 @@
 var logoText = "Trishna";
 var siteName = "Trishna";
-var bestLink = "https://wizardsdishwater.com/vs1x112dn0?key=28dcb4c360c99f75c117c61d0a61f495";
+var bestLink = "https://aff.bdoffers.tech/?utm_medium=00fc0215cf53394f7e2159add49fe4bd7db9f6a1&utm_campaign=Brazil";
 document.title = "Trishna New";
