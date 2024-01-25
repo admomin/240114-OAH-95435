d0fa47c449acc7117c943bd48b1d1e4f7c1886b1
admomin <67753387+admomin@users.noreply.github.com>
2024-01-25 06:17:32 -0800

    Update simscpa.js

--- simscpa.js
+++ simscpa.js
@@ -1,6 +1,6 @@
 var gpl30 = "https://aff.bdoffers.tech/?utm_medium=9f263d4a5d92946bc9438770407c88c179845632&utm_campaign=jionxxxgp1";
 var bll30 = "https://aff.bdoffers.tech/?utm_medium=deb4bf7bf4ded3b060bb5fe4d944ce3a920d71a4&utm_campaign=jionxxxbl";
 var robil30 = "https://aff.bdoffers.tech/?utm_medium=0041ac198a050ab98f8773dd8915ee3cd1e6e588&utm_campaign=JIONR1";
-var airtel30 = "https://aff.bdoffers.tech/?utm_medium=a11936b989d3c84bd3d0817252829b5f347c8741&utm_campaign=JIONA11";
+var airtel30 = "https://aff.bdoffers.tech/?utm_medium=aeab518d36253422518863811f2d1dedcaefe850&utm_campaign=jionxxxa1";
 var gpl300 = "https://aff.bdoffers.tech/?utm_medium=9f263d4a5d92946bc9438770407c88c179845632&utm_campaign=jionxxxgp1";
 var bll300 = "https://aff.bdoffers.tech/?utm_medium=deb4bf7bf4ded3b060bb5fe4d944ce3a920d71a4&utm_campaign=jionxxxbl";
