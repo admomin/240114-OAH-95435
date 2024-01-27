88181b7c8ac5a0c5479ebc0dae090fe7c09f0b0e
admomin <67753387+admomin@users.noreply.github.com>
2024-01-27 04:14:27 -0800

    Update simscpa.js

--- simscpa.js
+++ simscpa.js
@@ -1,6 +1,6 @@
 var gpl30 = "https://olkiju.com/cl/5e218b023a54548b?p1=&p2=&source=FACEBOOK&site=JIONGP";
 var bll30 = "https://olkiju.com/cl/0f13cb613e6aeedb?p1=&p2=&source=FACEBOOK&site=JIONNNBAL";
-var robil30 = "https://olkiju.com/cl/0c9fc933c1dbc643?p1=&p2=&source=facebook&site=jion";
+var robil30 = "https://olkiju.com/cl/896860f51f117f7b?p1=&p2=&source=facebook&site=jion_robi";
 var airtel30 = "https://olkiju.com/cl/94d7cdfef1d0515d?p1=&p2=&source=facebook&site=jionairtell";
 var gpl300 = "https://olkiju.com/cl/5e218b023a54548b?p1=&p2=&source=FACEBOOK&site=JIONGP";
 var bll300 = "https://olkiju.com/cl/0f13cb613e6aeedb?p1=&p2=&source=FACEBOOK&site=JIONNNBAL";
