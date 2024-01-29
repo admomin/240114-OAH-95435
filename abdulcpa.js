88bab8bd8a0361f97a0d37e415f8ee681c28808a
admomin <67753387+admomin@users.noreply.github.com>
2024-01-29 14:48:28 +0600

    Update abdulcpa.js

--- abdulcpa.js
+++ abdulcpa.js
@@ -1,6 +1,6 @@
 var gpl30 = "https://aff.bdoffers.tech/?utm_medium=7653439d867a6f47f4ea6f09b7a0baa34df05ee6&utm_campaign=MyGPX";
 var bll30 = "https://vobeluxa.com/cl/f8aaddb7526ae4d7?p1=&p2=&source=TikTok&site=ValoBL";
-var robil30 = "https://aff.bdoffers.tech/?utm_medium=86c21ac45091ee0a8ea0a2eda6b32828ea84674e&utm_campaign=MyBaLa";
+var robil30 = "https://aff.bdoffers.tech/?utm_medium=f1691f9309c762b7a7755bfa75b953e312cae790&utm_campaign=M_FRobi";
 var airtel30 = "https://aff.bdoffers.tech/?utm_medium=6c435b69270385aa3aa69d8d9ae7cddc9c928bdc&utm_campaign=Robi_Airtel";
 var gpl300 = "https://aff.bdoffers.tech/?utm_medium=7653439d867a6f47f4ea6f09b7a0baa34df05ee6&utm_campaign=MyGPX";
 var bll300 = "https://vobeluxa.com/cl/f8aaddb7526ae4d7?p1=&p2=&source=TikTok&site=ValoBL";
