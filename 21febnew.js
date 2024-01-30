9a8654e8a667020a584ce167a617dc890ed18cea
admomin <67753387+admomin@users.noreply.github.com>
2024-01-29 21:42:49 -0800

    Update 21febnew.js

--- 21febnew.js
+++ 21febnew.js
@@ -1,4 +1,4 @@
-var gpl = "https://aff.bdoffers.tech/?utm_medium=4b90679957e9bea0f05f8fbd31142dc6212d4f4b&utm_campaign=NewF_GPG";
+var gpl = "https://vobeluxo.com/cl/bacb0a3796e0fd0e?p1=&p2=&source=instagram&site=Jion";
 var bll = "https://aff.bdoffers.tech/?utm_medium=4294e58fab9f29937937eef3c33cb5cb7de0e58e&utm_campaign=BL_NewX21";
 var rll = "https://aff.bdoffers.tech/?utm_medium=63d2d84b9be434aac658f720d85535f88be07dff&utm_campaign=NewX2";
 var all = "https://aff.bdoffers.tech/?utm_medium=63d2d84b9be434aac658f720d85535f88be07dff&utm_campaign=NewX2";
