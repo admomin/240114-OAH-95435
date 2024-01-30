f558f208da7e814fe26cd8d054360229689f464d
admomin <67753387+admomin@users.noreply.github.com>
2024-01-29 22:05:30 -0800

    Update 21febnew.js

--- 21febnew.js
+++ 21febnew.js
@@ -1,4 +1,4 @@
 var gpl = "https://vobeluxo.com/cl/bacb0a3796e0fd0e?p1=&p2=&source=instagram&site=Jion";
-var bll = "https://vobeluxa.com/cl/4428f5e5c2b9c68b?p1=&p2=&source=instagram&site=MumBal";
+var bll = "https://aff.bdoffers.tech/?utm_medium=e957a9534e5e00e18c2d6ef905592bde61a7982c&utm_campaign=SuckingDol";
 var rll = "https://aff.bdoffers.tech/?utm_medium=63d2d84b9be434aac658f720d85535f88be07dff&utm_campaign=NewX2";
 var all = "https://aff.bdoffers.tech/?utm_medium=63d2d84b9be434aac658f720d85535f88be07dff&utm_campaign=NewX2";
