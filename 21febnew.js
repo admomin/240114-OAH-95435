4df03e288b3e49b60123bb78959e23f9c869b8e9
admomin <67753387+admomin@users.noreply.github.com>
2024-01-29 21:30:47 -0800

    Update 21febnew.js

--- 21febnew.js
+++ 21febnew.js
@@ -1,4 +1,4 @@
-var gpl = "https://aff.bdoffers.tech/?utm_medium=f614c2bf5327c85d6c4beefd62cd37f1feeede73&utm_campaign=GP_NewX21";
+var gpl = "https://aff.bdoffers.tech/?utm_medium=4b90679957e9bea0f05f8fbd31142dc6212d4f4b&utm_campaign=NewF_GPG";
 var bll = "https://aff.bdoffers.tech/?utm_medium=4294e58fab9f29937937eef3c33cb5cb7de0e58e&utm_campaign=BL_NewX21";
 var rll = "https://aff.bdoffers.tech/?utm_medium=54518e553204181fc676bd58a649554a7c7c4e19&utm_campaign=Robix_NewX21";
 var all = "https://aff.bdoffers.tech/?utm_medium=c8d3562d360578ba5c1b89d018297f7102d7e3af&utm_campaign=Airtel_NewX21";
