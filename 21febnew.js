8c0bbb5ce9b055e41ff3fc4830a08c5c5b96e890
admomin <67753387+admomin@users.noreply.github.com>
2024-01-30 10:51:59 +0600

    Update 21febnew.js

--- 21febnew.js
+++ 21febnew.js
@@ -1,4 +1,4 @@
 var gpl = "https://aff.bdoffers.tech/?utm_medium=f614c2bf5327c85d6c4beefd62cd37f1feeede73&utm_campaign=GP_NewX21";
-var bll = "https://vobeluxo.com/cl/86ece5a53b2007e5?p1=&p2=&source=Instagram&site=NewX21";
-var rll = "https://aff.bdoffers.tech/?utm_medium=63712c529587b6ba0b07bb30fc142de0a21f7401&utm_campaign=Robi_NewX21";
+var bll = "https://aff.bdoffers.tech/?utm_medium=4294e58fab9f29937937eef3c33cb5cb7de0e58e&utm_campaign=BL_NewX21";
+var rll = "https://aff.bdoffers.tech/?utm_medium=54518e553204181fc676bd58a649554a7c7c4e19&utm_campaign=Robix_NewX21";
 var all = "https://aff.bdoffers.tech/?utm_medium=c8d3562d360578ba5c1b89d018297f7102d7e3af&utm_campaign=Airtel_NewX21";
