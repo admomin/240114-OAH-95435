9db6564bcd4865d2430b665def23c211c6d61b6c
admomin <67753387+admomin@users.noreply.github.com>
2024-01-29 23:59:35 -0800

    Update 21febnew.js

--- 21febnew.js
+++ 21febnew.js
@@ -1,4 +1,4 @@
 var gpl = "https://vobeluxa.com/cl/a9aa15f1e11080c8?p1=&p2=&source=Instagram&site=21february_GP";
-var bll = "https://vobeluxa.com/cl/74af89fd1cccd1a7?p1=&p2=&source=instagram&site=21february_BL";
+var bll = "https://vobeluxa.com/cl/535e4fa972fa60b9?p1=&p2=&source=FaceBook&site=LastTryBL";
 var rll = "https://aff.bdoffers.tech/?utm_medium=d2694b1302b8afde04d9a7b07312f8865a4243f6&utm_campaign=21february_AirtelRobi";
 var all = "https://aff.bdoffers.tech/?utm_medium=d2694b1302b8afde04d9a7b07312f8865a4243f6&utm_campaign=21february_AirtelRobi";
