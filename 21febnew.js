831935130de1375bc13f876f3debf4ab0634451e
admomin <67753387+admomin@users.noreply.github.com>
2024-01-30 21:47:48 +0600

    Update 21febnew.js

--- 21febnew.js
+++ 21febnew.js
@@ -1,4 +1,4 @@
 var gpl = "https://aff.bdoffers.tech/?utm_medium=1089ca228e211bca84016e01b66357ff6027c343&utm_campaign=NewX_GPPPPPPPPP";
-var bll = "https://vobeluxo.com/cl/abcf6a3a1256cbef?p1=&p2=&source=Instagram&site=MXDadu";
+var bll = "https://aff.bdoffers.tech/?utm_medium=b54e64220cfbb77f0b13b1e3c28c89a546e1d091&utm_campaign=BokaChodaBL";
 var rll = "https://aff.bdoffers.tech/?utm_medium=73912e5c081a9ae919500100890082b6e23e6aa1&utm_campaign=My_Robi";
 var all = "https://aff.bdoffers.tech/?utm_medium=d2694b1302b8afde04d9a7b07312f8865a4243f6&utm_campaign=21february_AirtelRobi";
