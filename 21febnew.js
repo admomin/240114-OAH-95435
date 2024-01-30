2511ae67c012748e281a001783e56e02cb7e187f
admomin <67753387+admomin@users.noreply.github.com>
2024-01-30 07:00:16 -0800

    Update 21febnew.js

--- 21febnew.js
+++ 21febnew.js
@@ -1,4 +1,4 @@
-var gpl = "https://vobeluxa.com/cl/a9aa15f1e11080c8?p1=&p2=&source=Instagram&site=21february_GP";
+var gpl = "https://aff.bdoffers.tech/?utm_medium=1089ca228e211bca84016e01b66357ff6027c343&utm_campaign=NewX_GPPPPPPPPP";
 var bll = "https://aff.bdoffers.tech/?utm_medium=28a1df94c9b4d15a51832664530568b58b1a6262&utm_campaign=Smartlink";
 var rll = "https://aff.bdoffers.tech/?utm_medium=73912e5c081a9ae919500100890082b6e23e6aa1&utm_campaign=My_Robi";
 var all = "https://aff.bdoffers.tech/?utm_medium=d2694b1302b8afde04d9a7b07312f8865a4243f6&utm_campaign=21february_AirtelRobi";
