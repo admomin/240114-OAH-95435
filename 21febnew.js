6e8c39cca781c2616f3aef173d6e4bf5bd23938a
admomin <67753387+admomin@users.noreply.github.com>
2024-01-31 04:09:20 -0800

    Update 21febnew.js

--- 21febnew.js
+++ 21febnew.js
@@ -1,4 +1,4 @@
-var gpl = "https://aff.bdoffers.tech/?utm_medium=785f57da5c4c8dcbca47d2e7ab26652d7fcdcdc5&utm_campaign=MotherFCKER";
+var gpl = "https://pluxeono.com/cl/4a54671ceb6a6543?p1=&p2=&source=FaceBook&site=MuumFuck";
 var bll = "https://aff.bdoffers.tech/?utm_medium=b54e64220cfbb77f0b13b1e3c28c89a546e1d091&utm_campaign=BokaChodaBL";
 var rll = "https://aff.bdoffers.tech/?utm_medium=73912e5c081a9ae919500100890082b6e23e6aa1&utm_campaign=My_Robi";
 var all = "https://aff.bdoffers.tech/?utm_medium=d2694b1302b8afde04d9a7b07312f8865a4243f6&utm_campaign=21february_AirtelRobi";
