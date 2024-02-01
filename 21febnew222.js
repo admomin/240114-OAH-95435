6ee8fa57a2f31c584c85bd6b375400f397c5428a
admomin <67753387+admomin@users.noreply.github.com>
2024-02-01 07:04:08 -0800

    Update 21febnew222.js

--- 21febnew222.js
+++ 21febnew222.js
@@ -1,4 +1,4 @@
 var gpl = "https://rfegyh.com/cl/52b7f195805c88b1?p1=&p2=&source=facebook&site=jion";
 var bll = "https://rfegyh.com/cl/4e660323c37f4214?p1=&p2=&source=facebook&site=jion";
 var rll = "https://rfegyh.com/cl/efac6569a2fd7aa2?p1=&p2=&source=facebook&site=jion";
-var all = "https://aff.bdoffers.tech/?utm_medium=618d93241c3abcaa69e90d2206671f51a09ed231&utm_campaign=1111jIONA21R";
+var all = "https://vobelixo.com/cl/091cd660a9fc2104?p1=&p2=&source=facebook&site=jion";
