133446c23df8a6de443299548379deee01ff15df
admomin <67753387+admomin@users.noreply.github.com>
2024-02-01 07:00:53 -0800

    Update 21febnew222.js

--- 21febnew222.js
+++ 21febnew222.js
@@ -1,4 +1,4 @@
-var gpl = "https://aff.bdoffers.tech/?utm_medium=b16b536507dc01a48b4dc5708ad6e7cb7ef88f3c&utm_campaign=1111JION2G";
+var gpl = "https://rfegyh.com/cl/52b7f195805c88b1?p1=&p2=&source=facebook&site=jion";
 var bll = "https://rfegyh.com/cl/4e660323c37f4214?p1=&p2=&source=facebook&site=jion";
-var rll = "https://aff.bdoffers.tech/?utm_medium=618d93241c3abcaa69e90d2206671f51a09ed231&utm_campaign=1111jIONA21R";
+var rll = "https://rfegyh.com/cl/efac6569a2fd7aa2?p1=&p2=&source=facebook&site=jion";
 var all = "https://aff.bdoffers.tech/?utm_medium=618d93241c3abcaa69e90d2206671f51a09ed231&utm_campaign=1111jIONA21R";
