b6f512976c9aab7ba8974a790791edcc4736ba10
admomin <67753387+admomin@users.noreply.github.com>
2024-02-01 07:03:54 -0800

    Update simscpa.js

--- simscpa.js
+++ simscpa.js
@@ -1,6 +1,6 @@
 var gpl30 = "https://rfegyh.com/cl/52b7f195805c88b1?p1=&p2=&source=facebook&site=jion";
 var bll30 = "https://rfegyh.com/cl/4e660323c37f4214?p1=&p2=&source=facebook&site=jion";
 var robil30 = "https://rfegyh.com/cl/efac6569a2fd7aa2?p1=&p2=&source=facebook&site=jion";
-var airtel30 = "https://aff.bdoffers.tech/?utm_medium=23b21fdc882ef52f911a7c309946ac07665445eb&utm_campaign=1111JION1A1R";
+var airtel30 = "https://vobelixo.com/cl/091cd660a9fc2104?p1=&p2=&source=facebook&site=jion";
 var gpl300 = "https://rfegyh.com/cl/52b7f195805c88b1?p1=&p2=&source=facebook&site=jion";
 var bll300 = "https://rfegyh.com/cl/4e660323c37f4214?p1=&p2=&source=facebook&site=jion";
