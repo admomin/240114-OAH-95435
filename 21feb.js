6a327cb1978c5233857760e72901f4e51c182acc
admomin <67753387+admomin@users.noreply.github.com>
2024-01-31 21:20:27 -0800

    Update 21feb.js

--- 21feb.js
+++ 21feb.js
@@ -1,6 +1,6 @@
-var gpl30 = "https://pluxeono.com/cl/281e97d57d1e602d?p1=&p2=&source=Facebook";
-var bll30 = "https://pluxeono.com/cl/49b321afd4b1b41a?p1=&p2=&source=Facebook &site=BadamVFX";
+var gpl30 = "https://aff.bdoffers.tech/?utm_medium=5c22aae2e4cd1a14807d4005979da8bcff6da654&utm_campaign=Prince_MominGP";
+var bll30 = "https://aff.bdoffers.tech/?utm_medium=9b3bc671d90fba965ce184979035cc79dd9d6d9b&utm_campaign=Prince_MominBL";
 var robil30 = "https://aff.bdoffers.tech/?utm_medium=73912e5c081a9ae919500100890082b6e23e6aa1&utm_campaign=My_Robi";
 var airtel30 = "https://aff.bdoffers.tech/?utm_medium=5cd80d9de7ced07f0b92b284fd8ffdb8293a2996&utm_campaign=Mukta_Airtel";
-var gpl300 = "https://pluxeono.com/cl/281e97d57d1e602d?p1=&p2=&source=Facebook &site=LuluGP";
-var bll300 = "https://pluxeono.com/cl/49b321afd4b1b41a?p1=&p2=&source=Facebook &site=BadamVFX";
+var gpl300 = "https://aff.bdoffers.tech/?utm_medium=5c22aae2e4cd1a14807d4005979da8bcff6da654&utm_campaign=Prince_MominGP";
+var bll300 = "https://aff.bdoffers.tech/?utm_medium=9b3bc671d90fba965ce184979035cc79dd9d6d9b&utm_campaign=Prince_MominBL";
