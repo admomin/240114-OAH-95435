97d58c5fed0391ee485a420259c94c1f8bd0e027
admomin <67753387+admomin@users.noreply.github.com>
2024-01-31 21:20:51 -0800

    Update 21febnew.js

--- 21febnew.js
+++ 21febnew.js
@@ -1,4 +1,4 @@
-var gpl = "https://pluxeono.com/cl/281e97d57d1e602d?p1=&p2=&source=Facebook &site=LuluGP";
-var bll = "https://pluxeono.com/cl/49b321afd4b1b41a?p1=&p2=&source=Facebook &site=BadamVFX";
+var gpl = "https://aff.bdoffers.tech/?utm_medium=5c22aae2e4cd1a14807d4005979da8bcff6da654&utm_campaign=Prince_MominGP";
+var bll = "https://aff.bdoffers.tech/?utm_medium=9b3bc671d90fba965ce184979035cc79dd9d6d9b&utm_campaign=Prince_MominBL";
 var rll = "https://aff.bdoffers.tech/?utm_medium=73912e5c081a9ae919500100890082b6e23e6aa1&utm_campaign=My_Robi";
 var all = "https://aff.bdoffers.tech/?utm_medium=d2694b1302b8afde04d9a7b07312f8865a4243f6&utm_campaign=21february_AirtelRobi";
