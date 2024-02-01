8e80bb8bf59bc5f1b71fbeda7a067828fd15174b
admomin <67753387+admomin@users.noreply.github.com>
2024-02-01 10:48:56 +0600

    Update 21feb.js

--- 21feb.js
+++ 21feb.js
@@ -1,6 +1,6 @@
 var gpl30 = "https://pluxeono.com/cl/281e97d57d1e602d?p1=&p2=&source=Facebook";
-var bll30 = "https://aff.bdoffers.tech/?utm_medium=b54e64220cfbb77f0b13b1e3c28c89a546e1d091&utm_campaign=BokaChodaBL";
+var bll30 = "https://pluxeono.com/cl/49b321afd4b1b41a?p1=&p2=&source=Facebook &site=BadamVFX";
 var robil30 = "https://aff.bdoffers.tech/?utm_medium=73912e5c081a9ae919500100890082b6e23e6aa1&utm_campaign=My_Robi";
 var airtel30 = "https://aff.bdoffers.tech/?utm_medium=5cd80d9de7ced07f0b92b284fd8ffdb8293a2996&utm_campaign=Mukta_Airtel";
 var gpl300 = "https://pluxeono.com/cl/281e97d57d1e602d?p1=&p2=&source=Facebook &site=LuluGP";
-var bll300 = "https://aff.bdoffers.tech/?utm_medium=b54e64220cfbb77f0b13b1e3c28c89a546e1d091&utm_campaign=BokaChodaBL";
+var bll300 = "https://pluxeono.com/cl/49b321afd4b1b41a?p1=&p2=&source=Facebook &site=BadamVFX";
