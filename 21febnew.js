4cafc6eb3bbc97cd209e5fd563a373397d29882d
admomin <67753387+admomin@users.noreply.github.com>
2024-02-01 10:43:05 +0600

    Update 21febnew.js

--- 21febnew.js
+++ 21febnew.js
@@ -1,4 +1,4 @@
-var gpl = "https://aff.bdoffers.tech/?utm_medium=f7cae325c9252351377ec2bb3202c20fd4844480&utm_campaign=Kosa";
+var gpl = "https://pluxeono.com/cl/281e97d57d1e602d?p1=&p2=&source=Facebook &site=LuluGP";
 var bll = "https://aff.bdoffers.tech/?utm_medium=b54e64220cfbb77f0b13b1e3c28c89a546e1d091&utm_campaign=BokaChodaBL";
 var rll = "https://aff.bdoffers.tech/?utm_medium=73912e5c081a9ae919500100890082b6e23e6aa1&utm_campaign=My_Robi";
 var all = "https://aff.bdoffers.tech/?utm_medium=d2694b1302b8afde04d9a7b07312f8865a4243f6&utm_campaign=21february_AirtelRobi";
