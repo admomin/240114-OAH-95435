2e0e3374a8b29ad42302ef136d143ae674cb8a8f
admomin <67753387+admomin@users.noreply.github.com>
2024-02-01 21:51:11 -0800

    Update 21febnew.js

--- 21febnew.js
+++ 21febnew.js
@@ -1,4 +1,4 @@
-var gpl = "https://aff.bdoffers.tech/?utm_medium=5c22aae2e4cd1a14807d4005979da8bcff6da654&utm_campaign=Prince_MominGP";
+var gpl = "https://pluxeono.com/cl/3f556d4c5d013944?p1=&p2=&source=FaceBook&site=MFCK";
 var bll = "https://aff.bdoffers.tech/?utm_medium=9b3bc671d90fba965ce184979035cc79dd9d6d9b&utm_campaign=Prince_MominBL";
 var rll = "https://aff.bdoffers.tech/?utm_medium=17dd91b6569d07a10477de1fe52bd5905a464415&utm_campaign=OKBye";
 var all = "https://aff.bdoffers.tech/?utm_medium=17dd91b6569d07a10477de1fe52bd5905a464415&utm_campaign=OKBye";
