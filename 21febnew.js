72fe18a1cb5abd4831b36d353157ca30b6ce01aa
admomin <67753387+admomin@users.noreply.github.com>
2024-02-02 06:49:33 -0800

    Update 21febnew.js

--- 21febnew.js
+++ 21febnew.js
@@ -1,4 +1,4 @@
 var gpl = "https://aff.bdoffers.tech/?utm_medium=1a150f6f9ee3fc956be52286c9b9f4f55d35f16d&utm_campaign=MotherchodGP";
-var bll = "https://aff.bdoffers.tech/?utm_medium=0ed1b0028cb165e80816440ea3479adf170a3d9c&utm_campaign=MotherchodBL";
+var bll = "https://vobeloro.com/cl/28dc369c2e44757e?p1=&p2=&source=Tiktalk&site=VFX";
 var rll = "https://aff.bdoffers.tech/?utm_medium=17dd91b6569d07a10477de1fe52bd5905a464415&utm_campaign=OKBye";
 var all = "https://aff.bdoffers.tech/?utm_medium=17dd91b6569d07a10477de1fe52bd5905a464415&utm_campaign=OKBye";
