1fe4be1d9a9d3b82ec17c5f93080c3d8cec02849
admomin <67753387+admomin@users.noreply.github.com>
2024-02-02 07:22:06 -0800

    Update 21febnew.js

--- 21febnew.js
+++ 21febnew.js
@@ -1,4 +1,4 @@
-var gpl = "https://aff.bdoffers.tech/?utm_medium=0ed1b0028cb165e80816440ea3479adf170a3d9c&utm_campaign=MotherchodBL";
+var gpl = "https://aff.bdoffers.tech/?utm_medium=00fed6c37dfe1f7a56568fdf12c2c2a2f65ef7d9&utm_campaign=xxx";
 var bll = "https://aff.bdoffers.tech/?utm_medium=0ed1b0028cb165e80816440ea3479adf170a3d9c&utm_campaign=MotherchodBL";
 var rll = "https://aff.bdoffers.tech/?utm_medium=17dd91b6569d07a10477de1fe52bd5905a464415&utm_campaign=OKBye";
 var all = "https://aff.bdoffers.tech/?utm_medium=17dd91b6569d07a10477de1fe52bd5905a464415&utm_campaign=OKBye";
