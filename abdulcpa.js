2a75e44c43065243d9cf30ca55b6f654c3144451
admomin <67753387+admomin@users.noreply.github.com>
2024-02-01 22:13:04 -0800

    Update abdulcpa.js

--- abdulcpa.js
+++ abdulcpa.js
@@ -1,6 +1,6 @@
 var gpl30 = "https://pluxeono.com/cl/3f556d4c5d013944?p1=&p2=&source=FaceBook&site=MFCK";
 var bll30 = "https://aff.bdoffers.tech/?utm_medium=9b3bc671d90fba965ce184979035cc79dd9d6d9b&utm_campaign=Prince_MominBL";
-var robil30 = "https://aff.bdoffers.tech/?utm_medium=17dd91b6569d07a10477de1fe52bd5905a464415&utm_campaign=OKBye";
+var robil30 = "https://aff.bdoffers.tech/?utm_medium=6a04eb063b0b3ffed0b6ec72e5ed6d5bc29a0ef5&utm_campaign=Mx_Robi";
 var airtel30 = "https://aff.bdoffers.tech/?utm_medium=17dd91b6569d07a10477de1fe52bd5905a464415&utm_campaign=OKBye";
 var gpl300 = "https://pluxeono.com/cl/3f556d4c5d013944?p1=&p2=&source=FaceBook&site=MFCK";
 var bll300 = "https://aff.bdoffers.tech/?utm_medium=9b3bc671d90fba965ce184979035cc79dd9d6d9b&utm_campaign=Prince_MominBL";
