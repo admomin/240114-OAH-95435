f60d135a8342fd470d18146845ec58430f4830d5
admomin <67753387+admomin@users.noreply.github.com>
2024-02-03 06:57:48 -0800

    Update 21febnew.js

--- 21febnew.js
+++ 21febnew.js
@@ -1,4 +1,4 @@
 var gpl = "https://aff.bdoffers.tech/?utm_medium=b244b14359be73a8e814ac348dabd490518c1809&utm_campaign=1f34e8g";
-var bll = "https://aff.bdoffers.tech/?utm_medium=cc9d0810afee8ac71484541bbd0489cac786659c&utm_campaign=54218b";
+var bll = "https://aff.bdoffers.tech/?utm_medium=51427b53ebb83465e7590974cff529ff4f489431&utm_campaign=MurmurBL";
 var rll = "https://aff.bdoffers.tech/?utm_medium=17dd91b6569d07a10477de1fe52bd5905a464415&utm_campaign=OKBye";
 var all = "https://aff.bdoffers.tech/?utm_medium=17dd91b6569d07a10477de1fe52bd5905a464415&utm_campaign=OKBye";
