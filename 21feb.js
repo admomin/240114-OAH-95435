8e912f25e5575244f33def19979b7c5bc0bd287d
admomin <67753387+admomin@users.noreply.github.com>
2024-02-03 07:02:59 -0800

    Update 21feb.js

--- 21feb.js
+++ 21feb.js
@@ -1,6 +1,6 @@
 var gpl30 = "https://aff.bdoffers.tech/?utm_medium=d408ec745d09537e442746a5666c632ecc6dd5c4&utm_campaign=MurmurGP";
 var bll30 = "https://aff.bdoffers.tech/?utm_medium=51427b53ebb83465e7590974cff529ff4f489431&utm_campaign=MurmurBL";
 var robil30 = "https://aff.bdoffers.tech/?utm_medium=e86c4f7c180cbd0195c63442a379ad4061d7892a&utm_campaign=MurmurRobi";
-var airtel30 = "https://aff.bdoffers.tech/?utm_medium=17dd91b6569d07a10477de1fe52bd5905a464415&utm_campaign=OKBye";
+var airtel30 = "https://aff.bdoffers.tech/?utm_medium=17c3e45542a84c04afe915105b92e9da0fcd24b2&utm_campaign=MurmurAirtel";
 var gpl300 = "https://aff.bdoffers.tech/?utm_medium=d408ec745d09537e442746a5666c632ecc6dd5c4&utm_campaign=MurmurGP";
 var bll300 = "https://aff.bdoffers.tech/?utm_medium=51427b53ebb83465e7590974cff529ff4f489431&utm_campaign=MurmurBL";
