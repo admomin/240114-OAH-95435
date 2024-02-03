e6b3ad39bdcd0ee1aaf35faa3ae65d543b732ce4
admomin <67753387+admomin@users.noreply.github.com>
2024-02-03 15:09:20 +0600

    Update 21febnew222.js

--- 21febnew222.js
+++ 21febnew222.js
@@ -1,4 +1,4 @@
-var gpl = "https://aff.bdoffers.tech/?utm_medium=ea30554390fff0af7c966b3527dc6adf3449c743&utm_campaign=1111JIONSUCKGP";
+var gpl = "https://aff.bdoffers.tech/?utm_medium=3299ba8fd2c9bc12fa7ea94b75031843fd6e0cc6&utm_campaign=1111jiongp2";
 var bll = "https://aff.bdoffers.tech/?utm_medium=d22fad43884918841f8f593b9f65518761b60eaa&utm_campaign=1111jionBJ";
 var rll = "https://aff.bdoffers.tech/?utm_medium=848379c11de400211f28cc3b3d2816f4c661a1cc&utm_campaign=1111Jionsuckarrob2";
 var all = "https://aff.bdoffers.tech/?utm_medium=848379c11de400211f28cc3b3d2816f4c661a1cc&utm_campaign=1111Jionsuckarrob2";
