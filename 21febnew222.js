1ac4441e42c8e38e8a4221a125f08498b7148846
admomin <67753387+admomin@users.noreply.github.com>
2024-02-02 23:11:50 -0800

    Update 21febnew222.js

--- 21febnew222.js
+++ 21febnew222.js
@@ -1,4 +1,4 @@
 var gpl = "https://aff.bdoffers.tech/?utm_medium=ea30554390fff0af7c966b3527dc6adf3449c743&utm_campaign=1111JIONSUCKGP";
 var bll = "https://aff.bdoffers.tech/?utm_medium=d22fad43884918841f8f593b9f65518761b60eaa&utm_campaign=1111jionBJ";
-var rll = "https://aff.bdoffers.tech/?utm_medium=05905bc5e17c136d0224786a7e3e7b128361fe10&utm_campaign=1111JIONSUCKARROB";
+var rll = "https://aff.bdoffers.tech/?utm_medium=93753457d2ec66c13f872ddbfd54b4df3130ce87&utm_campaign=1111jionsuckrob";
 var all = "https://aff.bdoffers.tech/?utm_medium=05905bc5e17c136d0224786a7e3e7b128361fe10&utm_campaign=1111JIONSUCKARROB";
