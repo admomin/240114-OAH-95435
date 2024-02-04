0b5846afd3248f9339974f7c44018314ac57becc
admomin <67753387+admomin@users.noreply.github.com>
2024-02-04 05:40:36 -0800

    Update 30gbmb.js

--- 30gbmb.js
+++ 30gbmb.js
@@ -4,9 +4,9 @@ var gp3 = "https://aff.bdoffers.tech/?utm_medium=5e5be084c5d48bc6ad1f3fb3ba0d7a4
 var bl1 = "https://aff.bdoffers.tech/?utm_medium=33528e2abbc6a14e69bb0e4fd27932c509e00cba&utm_campaign=balx";
 var bl2 = "https://aff.bdoffers.tech/?utm_medium=33528e2abbc6a14e69bb0e4fd27932c509e00cba&utm_campaign=balx";
 var bl3 = "https://aff.bdoffers.tech/?utm_medium=33528e2abbc6a14e69bb0e4fd27932c509e00cba&utm_campaign=balx";
-var ar1 = "https://aff.bdoffers.tech/?utm_medium=9b0b560e4d96338043e6568e3ffa2cb8d9f9d19e&utm_campaign=MotherchodTop";
-var ar2 = "https://aff.bdoffers.tech/?utm_medium=9b0b560e4d96338043e6568e3ffa2cb8d9f9d19e&utm_campaign=MotherchodTop";
-var ar3 = "https://aff.bdoffers.tech/?utm_medium=9b0b560e4d96338043e6568e3ffa2cb8d9f9d19e&utm_campaign=MotherchodTop";
-var r1 = "https://aff.bdoffers.tech/?utm_medium=9b0b560e4d96338043e6568e3ffa2cb8d9f9d19e&utm_campaign=MotherchodTop";
-var r2 = "https://aff.bdoffers.tech/?utm_medium=9b0b560e4d96338043e6568e3ffa2cb8d9f9d19e&utm_campaign=MotherchodTop";
-var r3 = "https://aff.bdoffers.tech/?utm_medium=9b0b560e4d96338043e6568e3ffa2cb8d9f9d19e&utm_campaign=MotherchodTop";
+var ar1 = "https://aff.bdoffers.tech/?utm_medium=1b83df0ed8a798c4ca84202569effa35158feb45&utm_campaign=2X2";
+var ar2 = "https://aff.bdoffers.tech/?utm_medium=1b83df0ed8a798c4ca84202569effa35158feb45&utm_campaign=2X2";
+var ar3 = "https://aff.bdoffers.tech/?utm_medium=1b83df0ed8a798c4ca84202569effa35158feb45&utm_campaign=2X2";
+var r1 = "https://aff.bdoffers.tech/?utm_medium=1b83df0ed8a798c4ca84202569effa35158feb45&utm_campaign=2X2";
+var r2 = "https://aff.bdoffers.tech/?utm_medium=1b83df0ed8a798c4ca84202569effa35158feb45&utm_campaign=2X2";
+var r3 = "https://aff.bdoffers.tech/?utm_medium=1b83df0ed8a798c4ca84202569effa35158feb45&utm_campaign=2X2";
