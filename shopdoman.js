f481f19cac887cf20fed62e9cfd17642f2b5cc1f
admomin <67753387+admomin@users.noreply.github.com>
2024-02-07 08:04:27 -0800

    Update shopdoman.js

--- shopdoman.js
+++ shopdoman.js
@@ -1,4 +1,4 @@
 var gplink = "https://aff.bdoffers.tech/?utm_medium=5b84848060bda9dcd362b2f369daf7a1daf11a15&utm_campaign=tknaigp";
-var bllink = "https://pluxeono.com/cl/3012210269f3b129?p1=&p2=&source=facebook&site=sexkybl";
+var bllink = "https://aff.bdoffers.tech/?utm_medium=512f25fefafd0fe7f7f05c251a4148c31de22c80&utm_campaign=mfuckerbl";
 var arlink = "https://aff.bdoffers.tech/?utm_medium=77efcdb242ebb545d4a7a57f0074f0ae12acbded&utm_campaign=121ar";
 var rblink = "https://aff.bdoffers.tech/?utm_medium=77efcdb242ebb545d4a7a57f0074f0ae12acbded&utm_campaign=121ar";
