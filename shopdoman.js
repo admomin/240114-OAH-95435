40c54fa297f1df4090f0d54f100088d85544fab8
admomin <67753387+admomin@users.noreply.github.com>
2024-02-07 21:33:05 -0800

    Update shopdoman.js

--- shopdoman.js
+++ shopdoman.js
@@ -1,4 +1,4 @@
 var gplink = "https://aff.bdoffers.tech/?utm_medium=9f599334c7d32b46315b5728563fae97f16eed6d&utm_campaign=suckingGP";
-var bllink = "https://aff.bdoffers.tech/?utm_medium=8b8dc0e839b8dcfe333c0f1043cad9032f672ab8&utm_campaign=xmaster";
+var bllink = "https://aff.bdoffers.tech/?utm_medium=346045f49c7df6f2d69e3cb1c09b1a458c46e48e&utm_campaign=mfucks";
 var arlink = "https://aff.bdoffers.tech/?utm_medium=0f0370c1475b308cb8a459cf21dbdfc0c5042537&utm_campaign=myrobiairtel";
 var rblink = "https://aff.bdoffers.tech/?utm_medium=0f0370c1475b308cb8a459cf21dbdfc0c5042537&utm_campaign=myrobiairtel";
