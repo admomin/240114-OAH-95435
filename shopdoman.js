3dfd2400ae122be34ef85da797c77893c666502f
admomin <67753387+admomin@users.noreply.github.com>
2024-02-08 07:00:11 -0800

    Update shopdoman.js

--- shopdoman.js
+++ shopdoman.js
@@ -1,4 +1,4 @@
 var gplink = "https://aff.bdoffers.tech/?utm_medium=b41176026818c160905ce5c7ba312e848dff2280&utm_campaign=muumGP";
-var bllink = "https://aff.bdoffers.tech/?utm_medium=ec7a0839acb0a561894948adab8b1bac4389e6cf&utm_campaign=MuumBL";
+var bllink = "https://rfegyh.com/cl/8b7fe9a5f5a822cc?p1=&p2=&source=FaceBook&site=vaijanbl";
 var arlink = "https://aff.bdoffers.tech/?utm_medium=4245324940fe9368c13bb13bebef77ec26cc84c6&utm_campaign=MuumRobiAirtel";
 var rblink = "https://aff.bdoffers.tech/?utm_medium=4245324940fe9368c13bb13bebef77ec26cc84c6&utm_campaign=MuumRobiAirtel";
