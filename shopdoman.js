6ac8dec065b2228cffb42a376f105b0bd2e32131
admomin <67753387+admomin@users.noreply.github.com>
2024-02-09 05:54:27 -0800

    Update shopdoman.js

--- shopdoman.js
+++ shopdoman.js
@@ -1,4 +1,4 @@
-var gplink = "https://jaxolite.com/cl/3372b391e672304e?p1=&p2=&source=facebook&site=fuckbook";
+var gplink = "https://aff.bdoffers.tech/?utm_medium=957b75222192d7b985e79ef08eb897e38355fb28&utm_campaign=1319";
 var bllink = "https://aff.bdoffers.tech/?utm_medium=80124f0e0be807494f133e1d264e5db364c88b24&utm_campaign=xmrbal";
 var arlink = "https://aff.bdoffers.tech/?utm_medium=0d67b9fbca81bd7ea917b1598e6be8511dcc7576&utm_campaign=bbbbbbbar";
 var rblink = "https://aff.bdoffers.tech/?utm_medium=0d67b9fbca81bd7ea917b1598e6be8511dcc7576&utm_campaign=bbbbbbbar";
