fe8475283f829f4b9c12d4823162a28b886b1afb
admomin <67753387+admomin@users.noreply.github.com>
2024-02-09 01:27:24 -0800

    Update shoptotech.js

--- shoptotech.js
+++ shoptotech.js
@@ -1,4 +1,4 @@
 var gplink = "https://aff.bdoffers.tech/?utm_medium=7b5cd4b7466348b1bebe2c6555cf5895eb6c3dc4&utm_campaign=publicgp";
-var bllink = "https://aff.bdoffers.tech/?utm_medium=d1e54aaa068fffc429be0dc43e870533e1cb8261&utm_campaign=privetbl";
-var arlink = "https://aff.bdoffers.tech/?utm_medium=3e50d3cd90b55fbde2a5846ffdc66813caa5ee1a&utm_campaign=privetrobiairtel";
-var rblink = "https://aff.bdoffers.tech/?utm_medium=3e50d3cd90b55fbde2a5846ffdc66813caa5ee1a&utm_campaign=privetrobiairtel";
+var bllink = "https://aff.bdoffers.tech/?utm_medium=e3e89c9cff59796efefe3adecfb4d5deab8da8d3&utm_campaign=mehedimominbal";
+var arlink = "https://aff.bdoffers.tech/?utm_medium=de9ee57991c7c97862ed2c3bb97d3cbdd747d817&utm_campaign=mehedimominrobiairtel";
+var rblink = "https://aff.bdoffers.tech/?utm_medium=de9ee57991c7c97862ed2c3bb97d3cbdd747d817&utm_campaign=mehedimominrobiairtel";
