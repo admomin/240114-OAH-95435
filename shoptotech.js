9b258669f3ec65d7c866e0f7413aa6a9b33983db
admomin <67753387+admomin@users.noreply.github.com>
2024-02-11 02:20:20 -0800

    Update shoptotech.js

--- shoptotech.js
+++ shoptotech.js
@@ -1,4 +1,4 @@
 var gplink = "https://aff.bdoffers.tech/?utm_medium=88662f70167a6609d1ee51f4ea00bd24ee2c0076&utm_campaign=mpgp";
 var bllink = "https://aff.bdoffers.tech/?utm_medium=0cd2a809fa03fbc9be83ea39fa7ef78494f079ca&utm_campaign=mpbl";
-var arlink = "https://aff.bdoffers.tech/?utm_medium=ad5973e11cb5625891f996f3a2e16e1c38a0bc2f&utm_campaign=ggairtel";
-var rblink = "https://olkiju.com/cl/d3ed2113670963b5?p1=&p2=&source=FaceBook&site=Robi";
+var arlink = "https://aff.bdoffers.tech/?utm_medium=14d8b7aa1c66ffdf55d9024db9bb64b7697388b2&utm_campaign=mpairtelrobi";
+var rblink = "https://aff.bdoffers.tech/?utm_medium=14d8b7aa1c66ffdf55d9024db9bb64b7697388b2&utm_campaign=mpairtelrobi";
