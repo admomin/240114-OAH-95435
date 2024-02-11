177f040e685af2ea9badd268faf91ad246f39d50
admomin <67753387+admomin@users.noreply.github.com>
2024-02-11 10:21:25 -0800

    Update shopdoman.js

--- shopdoman.js
+++ shopdoman.js
@@ -1,4 +1,4 @@
 var gplink = "https://aff.bdoffers.tech/?utm_medium=9cffc9840001b6d2fb1171f62f2d4a4aa07423ef&utm_campaign=kanchonaGP";
 var bllink = "https://aff.bdoffers.tech/?utm_medium=845f1b106b20a6387c53dffe19731b470da10007&utm_campaign=kufabl";
-var arlink = "https://aff.bdoffers.tech/?utm_medium=32daad8d439b964cdba23d1520aca605c0b3f9b6&utm_campaign=kufara";
-var rblink = "https://aff.bdoffers.tech/?utm_medium=32daad8d439b964cdba23d1520aca605c0b3f9b6&utm_campaign=kufara";
+var arlink = "https://aff.bdoffers.tech/?utm_medium=a262b6e6a4616d610bff272b57c2c0d18e9ca18d&utm_campaign=kanchodaAirtelRobi";
+var rblink = "https://aff.bdoffers.tech/?utm_medium=a262b6e6a4616d610bff272b57c2c0d18e9ca18d&utm_campaign=kanchodaAirtelRobi";
