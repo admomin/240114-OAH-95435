4e9ab21972145b0c9926186008ee33e78b3a7fd0
admomin <67753387+admomin@users.noreply.github.com>
2024-02-12 09:22:17 +0600

    Update shoptotech.js

--- shoptotech.js
+++ shoptotech.js
@@ -1,4 +1,4 @@
 var gplink = "https://aff.bdoffers.tech/?utm_medium=2fe692c3427b7fae79999ba1712ed11c9dbeaeef&utm_campaign=12dfebgp";
 var bllink = "https://aff.bdoffers.tech/?utm_medium=c6fd4e084d9e2aebbc8ea12d6f136e2cdf6d923c&utm_campaign=12dfebbl";
-var arlink = "https://aff.bdoffers.tech/?utm_medium=7900318dad795b7db18de838d37f8969273d23a1&utm_campaign=12dfebar";
-var rblink = "https://aff.bdoffers.tech/?utm_medium=7900318dad795b7db18de838d37f8969273d23a1&utm_campaign=12dfebar";
+var arlink = "https://aff.bdoffers.tech/?utm_medium=6976f87d9a40a9835a3901436278d5e775d6f5bb&utm_campaign=m21b";
+var rblink = "https://aff.bdoffers.tech/?utm_medium=6976f87d9a40a9835a3901436278d5e775d6f5bb&utm_campaign=m21b";
