22963fa367fd06d1be177c771ad836f578537cd8
admomin <67753387+admomin@users.noreply.github.com>
2024-02-12 22:41:33 +0600

    Update shopdoman.js

--- shopdoman.js
+++ shopdoman.js
@@ -1,4 +1,4 @@
-var gplink = "https://aff.bdoffers.tech/?utm_medium=ae3936b48f68d381b9197be8a2c02f9040de15ea&utm_campaign=lailapg";
-var bllink = "https://aff.bdoffers.tech/?utm_medium=5677d69c8a081ce8f7eff77d3ee99113aa9198d7&utm_campaign=11bl";
+var gplink = "https://flibblix.com/cl/f32d516853a2294e?p1=&p2=&source=YouTube&site=camping1x";
+var bllink = "https://flibblix.com/cl/f32d516853a2294e?p1=&p2=&source=YouTube&site=camping1x";
 var arlink = "https://aff.bdoffers.tech/?utm_medium=00d31cd293b95e6e94c2e727d0bb9b682478e075&utm_campaign=11ar";
 var rblink = "https://aff.bdoffers.tech/?utm_medium=00d31cd293b95e6e94c2e727d0bb9b682478e075&utm_campaign=11ar";
