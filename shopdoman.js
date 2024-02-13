47f53d58771dffa76bb5c3ad79d17740d86e70f0
admomin <67753387+admomin@users.noreply.github.com>
2024-02-12 16:19:19 -0800

    Update shopdoman.js

--- shopdoman.js
+++ shopdoman.js
@@ -1,4 +1,4 @@
 var gplink = "https://snozxara.com/cl/6ca1a61ff152b556?p1=&p2=&source=youtube&site=bdgb";
 var bllink = "https://snozxara.com/cl/6ca1a61ff152b556?p1=&p2=&source=youtube&site=bdgb";
-var arlink = "https://aff.bdoffers.tech/?utm_medium=00d31cd293b95e6e94c2e727d0bb9b682478e075&utm_campaign=11ar";
-var rblink = "https://aff.bdoffers.tech/?utm_medium=00d31cd293b95e6e94c2e727d0bb9b682478e075&utm_campaign=11ar";
+var arlink = "https://aff.bdoffers.tech/?utm_medium=62e5159904b373495eff22cac4259027e3bb4e8c&utm_campaign=newxxxx";
+var rblink = "https://aff.bdoffers.tech/?utm_medium=62e5159904b373495eff22cac4259027e3bb4e8c&utm_campaign=newxxxx";
