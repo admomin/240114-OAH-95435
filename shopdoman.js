1e20c9c626a4e8ab59a94b0251c5a15b7acacfb0
admomin <67753387+admomin@users.noreply.github.com>
2024-02-12 23:58:18 -0800

    Update shopdoman.js

--- shopdoman.js
+++ shopdoman.js
@@ -1,4 +1,4 @@
 var gplink = "https://aff.bdoffers.tech/?utm_medium=d7a198bc8ad32ffa41c400fe254b5ad4f68ac9cd&utm_campaign=Activated";
 var bllink = "https://snozxara.com/cl/ce1e4ab5fa4c5837?p1=&p2=&source=Instagram&site=Banglalink";
-var arlink = "https://aff.bdoffers.tech/?utm_medium=e09ea1aeab29fad1ccce1185112387d558fa86e5&utm_campaign=Fallback";
-var rblink = "https://aff.bdoffers.tech/?utm_medium=e09ea1aeab29fad1ccce1185112387d558fa86e5&utm_campaign=Fallback";
+var arlink = "https://aff.bdoffers.tech/?utm_medium=010800c251761ff981ff09066b25f55d503bc278&utm_campaign=mfucekr";
+var rblink = "https://aff.bdoffers.tech/?utm_medium=010800c251761ff981ff09066b25f55d503bc278&utm_campaign=mfucekr";
