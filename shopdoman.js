cc7318a645951a79a4f826bb8d021f29192278d8
admomin <67753387+admomin@users.noreply.github.com>
2024-02-13 08:24:09 +0600

    Update shopdoman.js

--- shopdoman.js
+++ shopdoman.js
@@ -1,4 +1,4 @@
-var gplink = "https://snozxara.com/cl/efedc0559f9bcb11?p1=&p2=&source=Instagram &site=download";
+var gplink = "https://snozxara.com/cl/96cb2ff4d44f77bf?p1=&p2=&source=facebook&site=rxddz";
 var bllink = "https://snozxara.com/cl/96cb2ff4d44f77bf?p1=&p2=&source=facebook&site=rxddz";
 var arlink = "https://aff.bdoffers.tech/?utm_medium=f10da2ee630686099729c5b36b1d4cc1acc5155e&utm_campaign=hoinakan";
 var rblink = "https://aff.bdoffers.tech/?utm_medium=f10da2ee630686099729c5b36b1d4cc1acc5155e&utm_campaign=hoinakan";
