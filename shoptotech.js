0c9e8aac726233df7411e658e30a649c85e063b2
admomin <67753387+admomin@users.noreply.github.com>
2024-02-12 20:35:13 -0800

    Update shoptotech.js

--- shoptotech.js
+++ shoptotech.js
@@ -1,4 +1,4 @@
-var gplink = "https://snozxara.com/cl/96cb2ff4d44f77bf?p1=&p2=&source=facebook&site=rxddz";
-var bllink = "https://snozxara.com/cl/96cb2ff4d44f77bf?p1=&p2=&source=facebook&site=rxddz";
+var gplink = "https://snozxara.com/cl/8a0884ae0496e5c8?p1=&p2=&source=facebook&site=mumfck";
+var bllink = "https://snozxara.com/cl/8a0884ae0496e5c8?p1=&p2=&source=facebook&site=mumfck";
 var arlink = "https://aff.bdoffers.tech/?utm_medium=cac6cbcefdbf53096583d6592505cfe1f3dac531&utm_campaign=newxraaaaaa";
 var rblink = "https://aff.bdoffers.tech/?utm_medium=cac6cbcefdbf53096583d6592505cfe1f3dac531&utm_campaign=newxraaaaaa";
