5707fe915c8cd7b123dfce6fd6c4cd7c31d13d17
admomin <67753387+admomin@users.noreply.github.com>
2024-02-19 23:27:23 -0800

    Update shoptotech.js

--- shoptotech.js
+++ shoptotech.js
@@ -1,4 +1,4 @@
-var gplink = "https://zypholuxa.com/cl/bd0bcae2e91d95c8?p1=&p2=&source=facebook&site=trygp";
+var gplink = "https://aff.bdoffers.tech/?utm_medium=32b11658e29cd84bd69f0e4949f5f92e5c6f1898&utm_campaign=2fagpa";
 var bllink = "https://aff.bdoffers.tech/?utm_medium=a9547a3a45558a3041b297df48be8df5c172464d&utm_campaign=muminbalx";
 var arlink = "https://aff.bdoffers.tech/?utm_medium=d0e8a4e829c5e272a25b26958968d3be61d35488&utm_campaign=mixedar";
 var rblink = "https://aff.bdoffers.tech/?utm_medium=d0e8a4e829c5e272a25b26958968d3be61d35488&utm_campaign=mixedar";
