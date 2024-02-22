3b0514e1888d99b4d4156cce80b4efc112fc1e1a
admomin <67753387+admomin@users.noreply.github.com>
2024-02-22 05:52:16 -0800

    Update shopdoman.js

--- shopdoman.js
+++ shopdoman.js
@@ -1,4 +1,4 @@
 var gplink = "https://fynddyx.com/cl/0a9c3a388b3a2ddf?p1=&p2=&source=YouTube&site=Mxdd";
-var bllink = "https://snozxara.com/cl/8c1028ed3b56e349?p1=&p2=&source=instagram&site=Dj";
+var bllink = "https://aff.bdoffers.tech/?utm_medium=0c731bf82af31004ae6a7b599877bd72390b6d02&utm_campaign=38clickbal";
 var arlink = "https://snozxara.com/cl/14f35d203d3ae887?p1=&p2=&source=facebok&site=sd";
 var rblink = "https://snozxara.com/cl/14f35d203d3ae887?p1=&p2=&source=facebok&site=sd";
