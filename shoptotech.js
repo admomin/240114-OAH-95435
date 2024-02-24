73a1b9643d1eb529d72df225447012492741012b
admomin <67753387+admomin@users.noreply.github.com>
2024-02-24 01:07:01 -0800

    Update shoptotech.js

--- shoptotech.js
+++ shoptotech.js
@@ -1,4 +1,4 @@
-var gplink = "https://plivexx.com/cl/8a44ec102fb7681d?p1=&p2=&source=FaceBook&site=gpbl";
-var bllink = "https://plivexx.com/cl/8a44ec102fb7681d?p1=&p2=&source=FaceBook&site=gpbl";
+var gplink = "https://plivexx.com/cl/117cd9af9cef1653?p1=&p2=&source=FaceBook&site=gp";
+var bllink = "https://plivexx.com/cl/18a91934dcd38ff3?p1=&p2=&source=&site=Banglalink";
 var arlink = "https://aff.bdoffers.tech/?utm_medium=acb5a9ed3b435c89970fa339eaeb868875346f59&utm_campaign=techarirtellx";
 var rblink = "https://plivexx.com/cl/dd4fba78632df666?p1=&p2=&source=FaceBook&site=Robi";
