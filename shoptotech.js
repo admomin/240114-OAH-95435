72828d798dcc7f4cd8bb85e1c6ea71c16cb6f6cc
admomin <67753387+admomin@users.noreply.github.com>
2024-02-24 00:58:27 -0800

    Update shoptotech.js

--- shoptotech.js
+++ shoptotech.js
@@ -1,4 +1,4 @@
 var gplink = "https://plivexx.com/cl/8a44ec102fb7681d?p1=&p2=&source=FaceBook&site=gpbl";
 var bllink = "https://plivexx.com/cl/8a44ec102fb7681d?p1=&p2=&source=FaceBook&site=gpbl";
-var arlink = "https://plivexx.com/cl/eb34449b8739d009?p1=&p2=&source=FaceBook&site=Airtel";
+var arlink = "https://aff.bdoffers.tech/?utm_medium=acb5a9ed3b435c89970fa339eaeb868875346f59&utm_campaign=techarirtellx";
 var rblink = "https://plivexx.com/cl/dd4fba78632df666?p1=&p2=&source=FaceBook&site=Robi";
