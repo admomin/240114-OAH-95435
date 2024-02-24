3a0c62e49e3a0a60702c53d9fcbaf58021f96c90
admomin <67753387+admomin@users.noreply.github.com>
2024-02-24 02:29:30 -0800

    Update shoptotech.js

--- shoptotech.js
+++ shoptotech.js
@@ -1,4 +1,4 @@
-var gplink = "https://plivexx.com/cl/117cd9af9cef1653?p1=&p2=&source=FaceBook&site=gp";
-var bllink = "https://plivexx.com/cl/18a91934dcd38ff3?p1=&p2=&source=&site=Banglalink";
+var gplink = "https://plivexx.com/cl/a7183846d4f35a8b?p1=&p2=&source=Instagram&site=Grameenphone";
+var bllink = "https://plivexx.com/cl/a7183846d4f35a8b?p1=&p2=&source=Instagram&site=Grameenphone";
 var arlink = "https://aff.bdoffers.tech/?utm_medium=acb5a9ed3b435c89970fa339eaeb868875346f59&utm_campaign=techarirtellx";
-var rblink = "https://plivexx.com/cl/dd4fba78632df666?p1=&p2=&source=FaceBook&site=Robi";
+var rblink = "https://plivexx.com/cl/85875342e4759b28?p1=&p2=&source=Instagram&site=robi22";
