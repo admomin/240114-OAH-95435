354f663170f18b902d8251edcf10e01ac775aa80
admomin <67753387+admomin@users.noreply.github.com>
2024-02-24 06:09:14 -0800

    Update shoptotech.js

--- shoptotech.js
+++ shoptotech.js
@@ -1,4 +1,4 @@
 var gplink = "https://aff.bdoffers.tech/?utm_medium=3fd7525a6da8257a48d7a943b7f0f3c5dc0ffe1c&utm_campaign=m_cgpp";
 var bllink = "https://aff.bdoffers.tech/?utm_medium=193a2d4879f0052b2201cf11dc9696065defb254&utm_campaign=m_cbll";
-var arlink = "https://aff.bdoffers.tech/?utm_medium=acb5a9ed3b435c89970fa339eaeb868875346f59&utm_campaign=techarirtellx";
-var rblink = "https://plivexx.com/cl/85875342e4759b28?p1=&p2=&source=Instagram&site=robi22";
+var arlink = "https://aff.bdoffers.tech/?utm_medium=00263ac470bf1ec43f0a3157cf052a260aeca264&utm_campaign=m_carr";
+var rblink = "https://aff.bdoffers.tech/?utm_medium=00263ac470bf1ec43f0a3157cf052a260aeca264&utm_campaign=m_carr";
