23c80ccb215a352fe0077b77b39991e507130d95
admomin <67753387+admomin@users.noreply.github.com>
2024-02-25 18:05:00 -0800

    Update shoptotech.js

--- shoptotech.js
+++ shoptotech.js
@@ -1,4 +1,4 @@
-var gplink = "https://flibarox.com/cl/ce3b9d6ba783a5f3?p1=&p2=&source=instagram&site=mygp";
+var gplink = "https://aff.bdoffers.tech/?utm_medium=6a08c81ad72c4a3153c32edd9207a8592f35cc8a&utm_campaign=1319gp";
 var bllink = "https://flibarox.com/cl/c46f6bc5c382016c?p1=&p2=&source=instagram&site=mybl";
 var arlink = "https://aff.bdoffers.tech/?utm_medium=94a5c2f4ed7c367ffe17583fde679a3683392492&utm_campaign=meairtel";
 var rblink = "https://flibarox.com/cl/ed7f5e75e4df6eb7?p1=&p2=&source=Instagram&site=myrobi";
