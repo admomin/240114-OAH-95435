d498ed3399609ef8b50fd42ed8183aeb2021391d
admomin <67753387+admomin@users.noreply.github.com>
2024-02-26 10:59:53 +0600

    Create replaceshop.js

---
+++ replaceshop.js
@@ -0,0 +1,4 @@
+var gplink = "https://flibarox.com/cl/5dfe5ad297f900c7?p1=&p2=&source=instagram&site=GP1";
+var bllink = "https://flibarox.com/cl/4a09d43a4663ec54?p1=&p2=&source=instagram&site=BL1";
+var arlink = "https://flibarox.com/cl/39946053257d09aa?p1=&p2=&source=instagram&site=robi1";
+var rblink = "https://flibarox.com/cl/73df849258c1af2e?p1=&p2=&source=instagram&site=airtel1";
