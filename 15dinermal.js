a5a8ca31b066fc3ce877fbef217daec3faadef2e
admomin <67753387+admomin@users.noreply.github.com>
2024-02-26 22:07:06 +0600

    Create 15dinermal.js

---
+++ 15dinermal.js
@@ -0,0 +1,4 @@
+var gplink = "https://smrturl.co/o/584589/53385239?s1=xsms";
+var bllink = "https://smrturl.co/o/584589/53385239?s1=xsms";
+var arlink = "https://smrturl.co/o/584589/53385239?s1=xsms";
+var rblink = "https://smrturl.co/o/584589/53385239?s1=xsms";
