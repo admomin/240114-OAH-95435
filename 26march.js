07f58b084dd976587390fe22f95454afdf5a6a85
admomin <67753387+admomin@users.noreply.github.com>
2024-02-25 18:14:32 -0800

    Update 26march.js

--- 26march.js
+++ 26march.js
@@ -1,4 +1,4 @@
 var gplink = "https://aff.bdoffers.tech/?utm_medium=6a08c81ad72c4a3153c32edd9207a8592f35cc8a&utm_campaign=1319gp";
-var bllink = "https://flibarox.com/cl/c46f6bc5c382016c?p1=&p2=&source=instagram&site=mybl";
+var bllink = "https://aff.bdoffers.tech/?utm_medium=24665b7e0d7080df9ec4260a0d8ff1413206b614&utm_campaign=1319bal";
 var arlink = "https://aff.bdoffers.tech/?utm_medium=94a5c2f4ed7c367ffe17583fde679a3683392492&utm_campaign=meairtel";
-var rblink = "https://flibarox.com/cl/ed7f5e75e4df6eb7?p1=&p2=&source=Instagram&site=myrobi";
+var rblink = "https://aff.bdoffers.tech/?utm_medium=05c109ec252a62d1928e661c36b49b7c523cc123&utm_campaign=1319newrobi";
