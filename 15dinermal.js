58e7a4e48e6c20f3a3892342b50259efa40dd9ed
admomin <67753387+admomin@users.noreply.github.com>
2024-02-26 19:59:26 -0800

    Update 15dinermal.js

--- 15dinermal.js
+++ 15dinermal.js
@@ -1,4 +1,4 @@
-var gplink = "https://smrturl.co/o/584589/53350601?s1=mygp";
+var gplink = "https://smrturl.co/o/584589/53385239?s1=try24";
 var bllink = "https://smrturl.co/o/584589/53385239?s1=try24";
 var arlink = "https://smrturl.co/o/584589/53385239?s1=try24";
 var rblink = "https://smrturl.co/o/584589/53385239?s1=try24";
