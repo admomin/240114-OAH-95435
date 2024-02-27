5b53384198f2fcdee63db71fd33f21d7c2e201a2
admomin <67753387+admomin@users.noreply.github.com>
2024-02-26 22:05:31 -0800

    Update 15dinermal.js

--- 15dinermal.js
+++ 15dinermal.js
@@ -1,4 +1,4 @@
-var gplink = "https://aff.bdoffers.tech/?utm_medium=6dee22fd0baca54d09717b998c3a01f8f0bbc12b&utm_campaign=mxbgp";
-var bllink = "https://aff.bdoffers.tech/?utm_medium=92fdbf88bb28be71f43c7d837086484f8ebd0dc3&utm_campaign=mxbbl";
+var gplink = "https://aff.bdoffers.tech/?utm_medium=642489886c5a5309ff6f2130b8755c77288023c4&utm_campaign=dcgpx215";
+var bllink = "https://smrturl.co/o/584589/53385239?s1=banglalink";
 var arlink = "https://aff.bdoffers.tech/?utm_medium=0e80a0638d1cb311c35b426551af1ce60a301d6b&utm_campaign=mxbar";
 var rblink = "https://aff.bdoffers.tech/?utm_medium=0e80a0638d1cb311c35b426551af1ce60a301d6b&utm_campaign=mxbar";
