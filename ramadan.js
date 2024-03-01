4ac81ff60515c4a08c94bc50739af7b4b94ab8aa
admomin <67753387+admomin@users.noreply.github.com>
2024-02-29 20:42:04 -0800

    Update ramadan.js

--- ramadan.js
+++ ramadan.js
@@ -1,4 +1,4 @@
 var gplink = "https://aff.26march.shop/?utm_medium=3f178b3c7ae2cfff31d2917f76f959279f7a974c&utm_campaign=megp";
-var bllink = "https://smrturl.co/o/584589/53385239?s1=mala";
-var arlink = "https://smrturl.co/o/584589/53385239?s1=mala";
-var rblink = "https://smrturl.co/o/584589/53385239?s1=mala";
+var bllink = "https://smrturl.co/o/584589/53324451?s1=ducklisat";
+var arlink = "https://smrturl.co/o/584589/53324451?s1=ducklisat";
+var rblink = "https://smrturl.co/o/584589/53324451?s1=ducklisat";
