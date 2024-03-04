cb467f222fc3bf97d0f0d7a71ce45c6343cc8f75
admomin <67753387+admomin@users.noreply.github.com>
2024-03-04 01:08:39 -0800

    Update replace26march.js

--- replace26march.js
+++ replace26march.js
@@ -1,4 +1,4 @@
-var gplink = "https://aff.26march.shop/?utm_medium=d6728e9b4b3b519bd143bd6e9656e1e15b75bf8f&utm_campaign=jionsixgp";
-var bllink = "https://aff.26march.shop/?utm_medium=3e9d124e4cb9a621c7276c3ca09507ffdd5a2422&utm_campaign=jionsixbl";
+var gplink = "https://aff.26march.shop/?utm_medium=5925e90ff767a9a87bdd10c9b74ced62de0bb2e8&utm_campaign=jiongmumu";
+var bllink = "https://aff.26march.shop/?utm_medium=20b274fa4cadea620af32ff29b44ca4c15fcfa7f&utm_campaign=jionbmumu";
 var arlink = "https://aff.26march.shop/?utm_medium=7fb106114da3abe735bbb541fbc3b86982ee52fd&utm_campaign=jionsix";
 var rblink = "https://aff.26march.shop/?utm_medium=7fb106114da3abe735bbb541fbc3b86982ee52fd&utm_campaign=jionsix";
