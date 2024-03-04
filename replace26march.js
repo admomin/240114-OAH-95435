ec999f6b489c5dd29b6a3a20400d13d7306e99f2
admomin <67753387+admomin@users.noreply.github.com>
2024-03-04 10:41:03 -0800

    Update replace26march.js

--- replace26march.js
+++ replace26march.js
@@ -1,4 +1,4 @@
 var gplink = "https://smrturl.co/o/597804/53385229?s1=nambuu";
-var bllink = "https://smrturl.co/o/597804/53456437?s1=nambu";
-var arlink = "https://smrturl.co/o/597804/53456437?s1=nambu";
-var rblink = "https://smrturl.co/o/597804/53456437?s1=nambu";
+var bllink = "https://smrturl.co/o/597804/53385239?s1=jionsix";
+var arlink = "https://smrturl.co/o/597804/53385239?s1=jionsix";
+var rblink = "https://smrturl.co/o/597804/53385239?s1=jionsix";
