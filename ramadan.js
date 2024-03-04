7e844527d6f19ef4bd55fa7172a6b22ed700e761
admomin <67753387+admomin@users.noreply.github.com>
2024-03-04 15:26:27 +0600

    Update ramadan.js

--- ramadan.js
+++ ramadan.js
@@ -1,4 +1,4 @@
-var gplink = "https://smrturl.co/o/584589/53385229?s1=gpx";
+var gplink = "https://smrturl.co/o/584589/53385229?s1=muum";
 var bllink = "https://smrturl.co/o/584589/53385239?s1=mx";
 var arlink = "https://smrturl.co/o/584589/53385239?s1=mx";
 var rblink = "https://smrturl.co/o/584589/53385239?s1=mx";
