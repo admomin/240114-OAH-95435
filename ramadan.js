decf271d66490522a9656c5799703951d0a2541c
admomin <67753387+admomin@users.noreply.github.com>
2024-03-10 08:36:48 -0700

    Update ramadan.js

--- ramadan.js
+++ ramadan.js
@@ -1,4 +1,4 @@
 var gplink = "https://flibarox.com/cl/851bb2e4f41825d9?p1=&p2=&source=facebook&site=sd";
 var bllink = "https://flibarox.com/cl/2b7e4fc4c2d7e779?p1=&p2=&source=facebook&site=mybl";
-var arlink = "https://flibarox.com/cl/e3b7383a593dde24?p1=&p2=&source=facebok&site=mdairtel";
+var arlink = "https://smrturl.co/o/597804/53385239?s1=airtelxx";
 var rblink = "https://flibarox.com/cl/3843cd21c1b36318?p1=&p2=&source=facebook&site=mdrobi";
