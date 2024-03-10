8870a01643bbfbac2f98a730d0d7cc6cc16eec27
admomin <67753387+admomin@users.noreply.github.com>
2024-03-10 07:09:57 -0700

    Update ramadan.js

--- ramadan.js
+++ ramadan.js
@@ -1,4 +1,4 @@
-var gplink = "https://aff.26march.shop/?utm_medium=8f87e77d33e17fbebffef8bfa45e4d984647eb9c&utm_campaign=BGP";
+var gplink = "https://aff.26march.shop/?utm_medium=0b3efb4015234016a5ccfa2537fb1a00a512da3c&utm_campaign=mgpm";
 var bllink = "https://aff.26march.shop/?utm_medium=4da9ca5a2dabd32c78fa3aceffbc757e3dc6a3ad&utm_campaign=BBL";
-var arlink = "https://aff.26march.shop/?utm_medium=c288b4611824bcf833e28b3ea533e7dc0085f44f&utm_campaign=TTT";
-var rblink = "https://aff.26march.shop/?utm_medium=e795eefb7b024e740b703fdcb409ed53a5f55097&utm_campaign=XDTT";
+var arlink = "https://flibarox.com/cl/e3b7383a593dde24?p1=&p2=&source=facebok&site=mdairtel";
+var rblink = "https://flibarox.com/cl/3843cd21c1b36318?p1=&p2=&source=facebook&site=mdrobi";
