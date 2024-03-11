79fa26c6b51c728d60926242c848223edaa07811
admomin <67753387+admomin@users.noreply.github.com>
2024-03-11 16:36:07 +0600

    Update ramadan.js

--- ramadan.js
+++ ramadan.js
@@ -1,4 +1,4 @@
 var gplink = "https://plivexx.com/cl/3f124be989046e0d?p1=&p2=&source=youtube&site=BTC";
-var bllink = "https://plivexx.com/cl/11ba8bbdcbbe9daf?p1=&p2=&source=youtube&site=Banglalink";
+var bllink = "https://plivexx.com/cl/0f025c268cffb3d4?p1=&p2=&source=YouTube&site=MominarBal";
 var arlink = "https://aff.26march.shop/?utm_medium=791228476db73dc730395c84503ddaf1e701052d&utm_campaign=AnyTraffic";
 var rblink = "https://plivexx.com/cl/cf3912b715ff312c?p1=&p2=&source=youtube&site=XD";
