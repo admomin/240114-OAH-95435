525eae164b0034f7c7b756b5d0cd52c534e0ebff
admomin <67753387+admomin@users.noreply.github.com>
2024-03-11 02:33:46 -0700

    Update ramadan.js

--- ramadan.js
+++ ramadan.js
@@ -1,4 +1,4 @@
-var gplink = "https://smrturl.co/o/597804/53385229?s1=mobilecam";
+var gplink = "https://plivexx.com/cl/3f124be989046e0d?p1=&p2=&source=youtube&site=BTC";
 var bllink = "https://smrturl.co/o/597804/53385239?s1=momyall";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=momyall";
 var rblink = "https://smrturl.co/o/597804/53385239?s1=momyall";
