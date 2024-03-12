d5a3ea987ac08bdbd06ec6c06b65abe7c02abace
admomin <67753387+admomin@users.noreply.github.com>
2024-03-12 13:14:36 -0700

    Update ramadan.js

--- ramadan.js
+++ ramadan.js
@@ -1,4 +1,4 @@
-var gplink = "https://flibarox.com/cl/2fe5a307f05ad92c?p1=&p2=&source=instagram&site=d1";
+var gplink = "https://zypholuxa.com/cl/bb992ac5cd811562?p1=&p2=&source=instagram&site=Grameenphone";
 var bllink = "https://snozxara.com/cl/e541ce3835e93e5d?p1=&p2=&source=instagram&site=Bvideo";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=myx";
 var rblink = "https://flibarox.com/cl/dea4c158b456b1a8?p1=&p2=&source=instagram&site=DDD440";
