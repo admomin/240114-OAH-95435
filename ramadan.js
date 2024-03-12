b19a45c3d9435dac3705b9db8a3908ebced6fa39
admomin <67753387+admomin@users.noreply.github.com>
2024-03-12 16:04:57 -0700

    Update ramadan.js

--- ramadan.js
+++ ramadan.js
@@ -1,4 +1,4 @@
-var gplink = "https://zypholuxa.com/cl/a84a79b1fe65b930?p1=&p2=&source=YouTube&site=3gp";
+var gplink = "https://zypholuxa.com/cl/a6ec9e04febff8db?p1=&p2=&source=Instagram&site=vacu1";
 var bllink = "https://zypholuxa.com/cl/792d67aa9e8d6ce9?p1=&p2=&source=YouTube&site=mybal";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=vv";
 var rblink = "https://zypholuxa.com/cl/88e77a601bf4dade?p1=&p2=&source=YouTube&site=myrobi";
