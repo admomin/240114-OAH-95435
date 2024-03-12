845cd9a9e640cd38ef882983da4bd7c9f7a21a4d
admomin <67753387+admomin@users.noreply.github.com>
2024-03-12 08:05:58 -0700

    Update ramadan.js

--- ramadan.js
+++ ramadan.js
@@ -1,4 +1,4 @@
 var gplink = "https://flibarox.com/cl/2fe5a307f05ad92c?p1=&p2=&source=instagram&site=d1";
-var bllink = "https://flibarox.com/cl/232e88b6607a51f6?p1=&p2=&source=instagram&site=banglalink";
+var bllink = "https://snozxara.com/cl/e541ce3835e93e5d?p1=&p2=&source=instagram&site=Bvideo";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=myx";
 var rblink = "https://flibarox.com/cl/dea4c158b456b1a8?p1=&p2=&source=instagram&site=DDD440";
