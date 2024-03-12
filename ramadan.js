11384438010c500cbc9f986da367e5f7a6526d56
admomin <67753387+admomin@users.noreply.github.com>
2024-03-11 19:59:04 -0700

    Update ramadan.js

--- ramadan.js
+++ ramadan.js
@@ -1,4 +1,4 @@
-var gplink = "https://smrturl.co/o/597804/53385229?s1=googlegp";
+var gplink = "https://fynddyx.com/cl/a60a8bbedd93e3b9?p1=&p2=&source=YouTube&site=m1";
 var bllink = "https://smrturl.co/o/597804/53385239?s1=jisan";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=jisan";
 var rblink = "https://smrturl.co/o/597804/53385239?s1=jisan";
