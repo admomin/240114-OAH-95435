cffb39e02d3618294d73d6a47434f73d3af548ab
admomin <67753387+admomin@users.noreply.github.com>
2024-03-13 01:15:57 -0700

    Update ramadan.js

--- ramadan.js
+++ ramadan.js
@@ -1,4 +1,4 @@
-var gplink = "https://flibblix.com/cl/1f75dc63e7e9e1c8?p1=&p2=&source=Instagram&site=M2";
-var bllink = "https://zypholuxa.com/cl/792d67aa9e8d6ce9?p1=&p2=&source=YouTube&site=mybal";
+var gplink = "https://flibblix.com/cl/b033f1891f457834?p1=&p2=&source=instagram&site=fd";
+var bllink = "https://flibblix.com/cl/a045748f530da263?p1=&p2=&source=instagram&site=md";
 var arlink = "https://aff.26march.shop/?utm_medium=6b92482755cc7f0c1eb6b13f717044e3ca868697&utm_campaign=MS";
-var rblink = "https://flibblix.com/cl/a83ffd2edbee9c12?p1=&p2=&source=instagram&site=MominRobi";
+var rblink = "https://flibblix.com/cl/c0f830ec1f71a5d2?p1=&p2=&source=instagram&site=ds";
