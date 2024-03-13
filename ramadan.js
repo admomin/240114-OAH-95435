ca11d9b1c8fff5544df4ba89b5396752c779f0f8
admomin <67753387+admomin@users.noreply.github.com>
2024-03-13 15:56:58 -0700

    Update ramadan.js

--- ramadan.js
+++ ramadan.js
@@ -1,4 +1,4 @@
-var gplink = "https://snozxara.com/cl/eaa5da58926478e0?p1=&p2=&source=Instagram&site=GRAMEENPHONE";
+var gplink = "https://snozxara.com/cl/18abf15854793de2?p1=&p2=&source=instagram&site=Grameenphone";
 var bllink = "https://snozxara.com/cl/684bbd57fcb80494?p1=&p2=&source=instagram&site=balxxx";
 var arlink = "https://aff.26march.shop/?utm_medium=20c4f1c445551c2bd5e80a252a4ac0c3e64a40ec&utm_campaign=555";
 var rblink = "https://snozxara.com/cl/26c19b263d520eda?p1=&p2=&source=instagram&site=3aRobi";
