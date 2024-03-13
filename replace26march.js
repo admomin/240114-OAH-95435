cbebc8adf446d8b801b7af434a1cc8c13233eef5
admomin <67753387+admomin@users.noreply.github.com>
2024-03-13 17:17:05 +0600

    Update replace26march.js

--- replace26march.js
+++ replace26march.js
@@ -1,4 +1,4 @@
-var gplink = "https://aff.26march.shop/?utm_medium=bd52a83f27b78b8f5c3242d29cf9ed6c2d5e2d44&utm_campaign=Jiongpp";
+var gplink = "https://smrturl.co/o/597804/53385229?s1=jion_koreacudi";
 var bllink = "https://smrturl.co/o/597804/53385239?s1=jion_pok";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=jion_pok";
 var rblink = "https://smrturl.co/o/597804/53385239?s1=jion_pok";
