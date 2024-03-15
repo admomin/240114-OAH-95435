890960c5ad0268df5dd55b018f9274c34815c35d
admomin <67753387+admomin@users.noreply.github.com>
2024-03-15 10:49:55 +0600

    Update replace26march.js

--- replace26march.js
+++ replace26march.js
@@ -1,4 +1,4 @@
 var gplink = "https://zyvilix.com/cl/a77cf8168e04b44a?p1=&p2=&source=facebook&site=Jion";
-var bllink = "https://smrturl.co/o/597804/53385239?s1=jion_pok";
+var bllink = "https://zyvilix.com/cl/83c45e5625c4d421?p1=&p2=&source=facebook&site=Jion";
 var arlink = "https://zyvilix.com/cl/a89ae5bfd6fcf03d?p1=&p2=&source=facebook&site=Jion";
 var rblink = "https://zyvilix.com/cl/a89ae5bfd6fcf03d?p1=&p2=&source=facebook&site=Jion";
