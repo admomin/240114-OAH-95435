1294d870f110a84ed4b54c3d2b3d1c6f032a6c5c
admomin <67753387+admomin@users.noreply.github.com>
2024-03-15 11:00:42 +0600

    Update replace26march.js

--- replace26march.js
+++ replace26march.js
@@ -1,4 +1,4 @@
-var gplink = "https://zyvilix.com/cl/a77cf8168e04b44a?p1=&p2=&source=facebook&site=Jion";
+var gplink = "https://zyvilix.com/cl/58ba79fab9227f5c?p1=&p2=&source=facebook&site=Jion";
 var bllink = "https://zyvilix.com/cl/83c45e5625c4d421?p1=&p2=&source=facebook&site=Jion";
 var arlink = "https://zyvilix.com/cl/a89ae5bfd6fcf03d?p1=&p2=&source=facebook&site=Jion";
 var rblink = "https://zyvilix.com/cl/a89ae5bfd6fcf03d?p1=&p2=&source=facebook&site=Jion";
