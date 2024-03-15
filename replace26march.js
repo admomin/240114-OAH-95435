ab1e1527b42df512630b97b7e507e1f5e49c1eb0
admomin <67753387+admomin@users.noreply.github.com>
2024-03-15 16:44:38 +0600

    Update replace26march.js

--- replace26march.js
+++ replace26march.js
@@ -1,4 +1,4 @@
 var gplink = "https://zyvilix.com/cl/6d53e877d7550917?p1=&p2=&source=facebook&site=Jiom";
-var bllink = "https://smrturl.co/o/597804/53385239?s1=jionsaheb";
-var arlink = "https://smrturl.co/o/597804/53385239?s1=jionsaheb";
+var bllink = "https://zyvilix.com/cl/6fb2f937ac10b2c4?p1=&p2=&source=facebook&site=Jion";
+var arlink = "https://zyvilix.com/cl/6fb2f937ac10b2c4?p1=&p2=&source=facebook&site=Jion";
 var rblink = "https://zyvilix.com/cl/b4fbd4dce3affefc?p1=&p2=&source=facebook&site=Jion";
