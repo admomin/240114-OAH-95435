40e7438787969d0ef734aca034821a039b8e1bfe
admomin <67753387+admomin@users.noreply.github.com>
2024-03-15 11:08:31 +0600

    Update replace26march.js

--- replace26march.js
+++ replace26march.js
@@ -1,4 +1,4 @@
 var gplink = "https://zyvilix.com/cl/58ba79fab9227f5c?p1=&p2=&source=facebook&site=Jion";
-var bllink = "https://zyvilix.com/cl/83c45e5625c4d421?p1=&p2=&source=facebook&site=Jion";
-var arlink = "https://zyvilix.com/cl/a89ae5bfd6fcf03d?p1=&p2=&source=facebook&site=Jion";
-var rblink = "https://zyvilix.com/cl/a89ae5bfd6fcf03d?p1=&p2=&source=facebook&site=Jion";
+var bllink = "https://smrturl.co/o/597804/53385239?s1=jionsaheb";
+var arlink = "https://smrturl.co/o/597804/53385239?s1=jionsaheb";
+var rblink = "https://smrturl.co/o/597804/53385239?s1=jionsaheb";
