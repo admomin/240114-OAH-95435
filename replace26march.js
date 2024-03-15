15490a51e63e8e43ced600c1a81f0b7854943fee
admomin <67753387+admomin@users.noreply.github.com>
2024-03-15 17:03:53 +0600

    Update replace26march.js

--- replace26march.js
+++ replace26march.js
@@ -1,4 +1,4 @@
-var gplink = "https://zyvilix.com/cl/6d53e877d7550917?p1=&p2=&source=facebook&site=Jiom";
-var bllink = "https://zyvilix.com/cl/6fb2f937ac10b2c4?p1=&p2=&source=facebook&site=Jion";
+var gplink = "https://zyvilix.com/cl/203b1343fbf70973?p1=&p2=&source=facebook&site=Jion";
+var bllink = "https://zyvilix.com/cl/203b1343fbf70973?p1=&p2=&source=facebook&site=Jion";
 var arlink = "https://zyvilix.com/cl/6fb2f937ac10b2c4?p1=&p2=&source=facebook&site=Jion";
 var rblink = "https://zyvilix.com/cl/b4fbd4dce3affefc?p1=&p2=&source=facebook&site=Jion";
