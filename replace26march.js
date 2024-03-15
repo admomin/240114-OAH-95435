c70c15257eb04a5e9ba60c75e3b9986c2212e5f9
admomin <67753387+admomin@users.noreply.github.com>
2024-03-15 11:13:55 +0600

    Update replace26march.js

--- replace26march.js
+++ replace26march.js
@@ -1,4 +1,4 @@
-var gplink = "https://zyvilix.com/cl/58ba79fab9227f5c?p1=&p2=&source=facebook&site=Jion";
+var gplink = "https://smrturl.co/o/597804/53385229?s1=jion";
 var bllink = "https://smrturl.co/o/597804/53385239?s1=jionsaheb";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=jionsaheb";
 var rblink = "https://smrturl.co/o/597804/53385239?s1=jionsaheb";
