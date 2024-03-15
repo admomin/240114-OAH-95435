d00e785dd82a75e982df754b7b1ac0c707d4bbaa
admomin <67753387+admomin@users.noreply.github.com>
2024-03-15 15:42:46 +0600

    Update replace26march.js

--- replace26march.js
+++ replace26march.js
@@ -1,4 +1,4 @@
-var gplink = "https://smrturl.co/o/597804/53385229?s1=jion";
+var gplink = "https://smrturl.co/o/597804/53385229?s1=jixsix";
 var bllink = "https://smrturl.co/o/597804/53385239?s1=jionsaheb";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=jionsaheb";
 var rblink = "https://smrturl.co/o/597804/53385239?s1=jionsaheb";
