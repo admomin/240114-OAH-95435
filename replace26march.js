ae2889048181a298294f68680b6395df466ba822
admomin <67753387+admomin@users.noreply.github.com>
2024-03-15 16:38:02 +0600

    Update replace26march.js

--- replace26march.js
+++ replace26march.js
@@ -1,4 +1,4 @@
 var gplink = "https://smrturl.co/o/597804/53385229?s1=jixsix";
 var bllink = "https://smrturl.co/o/597804/53385239?s1=jionsaheb";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=jionsaheb";
-var rblink = "https://smrturl.co/o/597804/53385239?s1=jionsaheb";
+var rblink = "https://zyvilix.com/cl/b4fbd4dce3affefc?p1=&p2=&source=facebook&site=Jion";
