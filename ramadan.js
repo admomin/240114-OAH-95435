3348d80ff1eba834cfb6df353f73b16d0228062f
admomin <67753387+admomin@users.noreply.github.com>
2024-03-15 22:31:12 -0700

    Update ramadan.js

--- ramadan.js
+++ ramadan.js
@@ -1,4 +1,4 @@
 var gplink = "https://hpmyrik.com/cl/1723f6316f5ae5c0?p1=&p2=&source=instagram&site=xgp";
 var bllink = "https://hpmyrik.com/cl/b45b2b1e86922f0f?p1=&p2=&source=instagram&site=mxbal";
-var arlink = "https://aff.26march.shop/?utm_medium=852c0354ef981c944b904f8020a8752fd377ab02&utm_campaign=myairtel";
+var arlink = "https://aff.26march.shop/?utm_medium=f76f531647a15e8a33bbf6b69d01ff69f5915028&utm_campaign=ddr4x";
 var rblink = "https://hpmyrik.com/cl/636e5670d78d0df2?p1=&p2=&source=youtube&site=mydkrobi";
