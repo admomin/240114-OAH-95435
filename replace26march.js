fb5d8f54a3d3947ba49161130f745fcdab3c0227
admomin <67753387+admomin@users.noreply.github.com>
2024-03-15 22:13:18 -0600

    Update replace26march.js

--- replace26march.js
+++ replace26march.js
@@ -1,4 +1,4 @@
 var gplink = "https://aff.26march.shop/?utm_medium=6dcb41979c6f665d07f6c5c699815d591cc9903d&utm_campaign=jiongp10";
-var bllink = "https://aff.26march.shop/?utm_medium=3ae73d13e34d79a64982cbfb616f0ea5f54d7adb&utm_campaign=jionbal011";
-var arlink = "https://aff.26march.shop/?utm_medium=60af88fa1854c1ebdd236170e6c0ad769d4d3bac&utm_campaign=Jionarrob2727";
-var rblink = "https://aff.26march.shop/?utm_medium=60af88fa1854c1ebdd236170e6c0ad769d4d3bac&utm_campaign=Jionarrob2727";
+var bllink = "https://smrturl.co/o/597804/53385239?s1=jionvI";
+var arlink = "https://smrturl.co/o/597804/53385239?s1=jionvI";
+var rblink = "https://smrturl.co/o/597804/53385239?s1=jionvI";
