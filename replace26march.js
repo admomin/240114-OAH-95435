6e117c55eb4b1326ca593962bf150138072a07f5
admomin <67753387+admomin@users.noreply.github.com>
2024-03-15 22:14:14 -0600

    Update replace26march.js

--- replace26march.js
+++ replace26march.js
@@ -1,4 +1,4 @@
-var gplink = "https://aff.26march.shop/?utm_medium=6dcb41979c6f665d07f6c5c699815d591cc9903d&utm_campaign=jiongp10";
+var gplink = "https://smrturl.co/o/597804/53385229?s1=JIONGP";
 var bllink = "https://smrturl.co/o/597804/53385239?s1=jionvI";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=jionvI";
 var rblink = "https://smrturl.co/o/597804/53385239?s1=jionvI";
