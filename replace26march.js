bc0e71b3a593f3f23188172019c749dd9b84dea2
admomin <67753387+admomin@users.noreply.github.com>
2024-03-15 22:45:34 -0600

    Update replace26march.js

--- replace26march.js
+++ replace26march.js
@@ -1,4 +1,4 @@
-var gplink = "https://smrturl.co/o/597804/53385229?s1=JIONGP";
+var gplink = "https://aff.26march.shop/?utm_medium=804703fd81ab6ca031326de12bab14885a776afb&utm_campaign=jiongppppppp";
 var bllink = "https://smrturl.co/o/597804/53385239?s1=jionvI";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=jionvI";
 var rblink = "https://smrturl.co/o/597804/53385239?s1=jionvI";
