87302c3484eaeb49f15907ccff04d84e7027fce1
admomin <67753387+admomin@users.noreply.github.com>
2024-03-15 22:57:27 -0700

    Update ramadan.js

--- ramadan.js
+++ ramadan.js
@@ -1,4 +1,4 @@
-var gplink = "https://smrturl.co/o/597804/53385229?s1=fuckerofjion";
-var bllink = "https://aff.26march.shop/?utm_medium=c87d9a0869d20a3d953156bc6e18e74736d3bbf7&utm_campaign=gaitaebal";
+var gplink = "https://hpmyrik.com/cl/ef9aa288e5124046?p1=&p2=&source=YouTube&site=mygp";
+var bllink = "https://hpmyrik.com/cl/c73572339dfafffd?p1=&p2=&source=YouTube&site=mbal";
 var arlink = "https://aff.26march.shop/?utm_medium=f76f531647a15e8a33bbf6b69d01ff69f5915028&utm_campaign=ddr4x";
-var rblink = "https://aff.26march.shop/?utm_medium=ccaebc0744df37b068e1aeee42c6d9547127a185&utm_campaign=drobinew";
+var rblink = "https://hpmyrik.com/cl/d8f69f6d6c6ff9e6?p1=&p2=&source=YouTube&site=jrobi";
