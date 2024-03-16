cddbed1c887d7c70449269908a356a5ed4a24b56
admomin <67753387+admomin@users.noreply.github.com>
2024-03-15 22:49:36 -0600

    Update replace26march.js

--- replace26march.js
+++ replace26march.js
@@ -1,4 +1,4 @@
 var gplink = "https://aff.26march.shop/?utm_medium=804703fd81ab6ca031326de12bab14885a776afb&utm_campaign=jiongppppppp";
 var bllink = "https://aff.26march.shop/?utm_medium=a239b1ee3f622d43870c30814fd1d52468aea14b&utm_campaign=jionnblllll";
-var arlink = "https://smrturl.co/o/597804/53385239?s1=jionvI";
+var arlink = "https://aff.26march.shop/?utm_medium=fc556beff7d8f0af86d3f6eb9279289789ab2f1d&utm_campaign=jionarrrrrr";
 var rblink = "https://aff.26march.shop/?utm_medium=1d5ba9b7aecef55b47a20ab1ad3aff3524c99e28&utm_campaign=jionro";
