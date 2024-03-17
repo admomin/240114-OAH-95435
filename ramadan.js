29d07f58789ec16a6543782396c05a4acc92d0da
admomin <67753387+admomin@users.noreply.github.com>
2024-03-17 01:13:52 -0700

    Update ramadan.js

--- ramadan.js
+++ ramadan.js
@@ -1,4 +1,4 @@
-var gplink = "https://aff.26march.shop/?utm_medium=055be101d9593d0d1d3cb5a3623a6a2c39b49150&utm_campaign=30secoud";
+var gplink = "https://aff.26march.shop/?utm_medium=0734f488e87ad0d9d6e4f13822c100543bc6466a&utm_campaign=gpc";
 var bllink = "https://aff.26march.shop/?utm_medium=0dff8660076c899deb30be6befb7d93edf175226&utm_campaign=90bal";
 var arlink = "https://aff.26march.shop/?utm_medium=568095c8d76daac5330e59684819184290a4c076&utm_campaign=22airtelx";
 var rblink = "https://aff.26march.shop/?utm_medium=568095c8d76daac5330e59684819184290a4c076&utm_campaign=22airtelx";
