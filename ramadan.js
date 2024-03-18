5408b329d704022c0101fd89c4310c3c3d21d081
admomin <67753387+admomin@users.noreply.github.com>
2024-03-18 08:22:36 -0700

    Update ramadan.js

--- ramadan.js
+++ ramadan.js
@@ -1,4 +1,4 @@
 var gplink = "https://flibblix.com/cl/2c7fd720a61ca3ad?p1=&p2=&source=instagram&site=megp";
-var bllink = "https://flibblix.com/cl/d476cd55c7da8551?p1=&p2=&source=instagram&site=banglali";
+var bllink = "https://flibblix.com/cl/679cbb82ef102d05?p1=&p2=&source=Instagram&site=myball";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=v";
-var rblink = "https://flibblix.com/cl/c3ebe3d6b6c417c4?p1=&p2=&source=instagram&site=robinew1010";
+var rblink = "https://flibblix.com/cl/7363e71cc06e9ceb?p1=&p2=&source=Instagram &site=myrobia";
