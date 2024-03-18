b867b55ca649c0e3c259bf10bac5286f465c0535
admomin <67753387+admomin@users.noreply.github.com>
2024-03-18 03:13:02 -0700

    Update ramadan.js

--- ramadan.js
+++ ramadan.js
@@ -1,4 +1,4 @@
-var gplink = "https://flibblix.com/cl/7b4947f2ec959fec?p1=&p2=&source=instagram&site=gpsim";
+var gplink = "https://hpmyrik.com/cl/5958438e8b09135e?p1=&p2=&source=instagram&site=mygp";
 var bllink = "https://flibblix.com/cl/d476cd55c7da8551?p1=&p2=&source=instagram&site=banglali";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=v";
 var rblink = "https://flibblix.com/cl/c3ebe3d6b6c417c4?p1=&p2=&source=instagram&site=robinew1010";
