5253294b4dfe345cc4aac83b2f2731d822d6328a
admomin <67753387+admomin@users.noreply.github.com>
2024-03-19 12:45:08 -0500

    Update replace26march.js

--- replace26march.js
+++ replace26march.js
@@ -1,4 +1,4 @@
 var gplink = "https://hpmyrik.com/cl/8d5eda81dd7b9480?p1=&p2=&source=facbook&site=jion";
-var bllink = "https://smrturl.co/o/597804/53385239?s1=sawwajion";
+var bllink = "https://hpmyrik.com/cl/d0cfc4882b1bc7e2?p1=&p2=&source=facbook&site=jion";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=sawwajion";
 var rblink = https://smrturl.co/o/597804/53385239?s1=sawwajion";
