c421a9a6a0cb1afa7c3380e4ce8ab8ee039fcf7e
admomin <67753387+admomin@users.noreply.github.com>
2024-03-19 12:44:12 -0500

    Update replace26march.js

--- replace26march.js
+++ replace26march.js
@@ -1,4 +1,4 @@
-var gplink = "https://smrturl.co/o/597804/53385229?s1=jiongpp";
+var gplink = "https://hpmyrik.com/cl/8d5eda81dd7b9480?p1=&p2=&source=facbook&site=jion";
 var bllink = "https://smrturl.co/o/597804/53385239?s1=sawwajion";
 var arlink = "https://smrturl.co/o/597804/53385239?s1=sawwajion";
 var rblink = https://smrturl.co/o/597804/53385239?s1=sawwajion";
